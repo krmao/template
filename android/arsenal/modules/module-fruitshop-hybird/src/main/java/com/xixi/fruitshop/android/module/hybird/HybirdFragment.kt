86cbb174d0b77cba3f8e4eb439f39335a6f12013
maokangren <maokangren@chexiang.com>
2017-09-25 18:15:56 +0800

    [android] add webview page

--- android/arsenal/modules/module-fruitshop-hybird/src/main/java/com/xixi/fruitshop/android/module/hybird/HybirdFragment.kt
+++ android/arsenal/modules/module-fruitshop-hybird/src/main/java/com/xixi/fruitshop/android/module/hybird/HybirdFragment.kt
@@ -5,10 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.xixi.library.android.base.FSBaseFragment
+import com.xixi.library.android.widget.webview.FSWebFragmentV2
+import kotlinx.android.synthetic.main.hybird_fragment.*
 
 class HybirdFragment : FSBaseFragment() {
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater?.inflate(R.layout.hybird_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        text.setOnClickListener {
+            FSWebFragmentV2.goTo(activity, "https://www.baidu.com")
+        }
     }
 }

9b8a9d2df4845d01dfa025ffb2091fb9dfcda11d
michael <767709667@qq.com>
2017-10-10 09:51:58 +0800

    [IOS]del pods

--- apps/app-house-keeper/html/src/router/index.js
+++ apps/app-house-keeper/html/src/router/index.js
@@ -5,12 +5,21 @@ import B from '@/components/B/B'
 
 Vue.use(Router)
 
-export default new Router({
+let router = new Router({
+    mode: 'hash',//hash abstract
+    scrollBehavior(to, from, savedPosition) {
+        return savedPosition || {x: 0, y: 0}
+    },
     routes: [
         {
             path: '/',
             name: 'A',
-            component: A
+            component: A,
+            // beforeEnter: function (to, from, next) {
+            //     console.log("[routerA]:beforeEnter:to", to)
+            //     console.log("[routerA]:beforeEnter:from", from)
+            //     next()
+            // },
         },
         {
             path: '/B',
@@ -23,35 +32,24 @@ export default new Router({
                 window.location = "https://c.chexiang.com/promotion/201709xinche.htm?channel=cx_zc_201709_cxyx_h5"
             }
         }
-    ],
-    beforeEach: function () {
-
-    },
-    beforeResolve: function () {
-
-    } ,
-    afterEach: function () {
-
-    } ,
-    onReady: function () {
-
-    }
-    ,
-    onError: function () {
-
-    }
-    ,
-    forward: function () {
-
-    },
-    go: function () {
-
-    }, push: function () {
-
-    },
-    back: function () {
-
-    }, replace: function () {
-
-    }
+    ]
 })
+
+// router.beforeEach((to, from, next) => {
+// console.log("[router2]:beforeEach:to", to)
+// console.log("[router2]:beforeEach:from", from)
+// next()
+// })
+
+// router.beforeResolve((to, from, next) => {
+// console.log("[router2]:beforeResolve:to", to)
+// console.log("[router2]:beforeResolve:from", from)
+// next()
+// })
+// router.afterEach((to, from) => {
+// console.log("[router2]:afterEach:to", to)
+// console.log("[router2]:afterEach:from", from)
+//     to.matched[0].components.default.methods.onGoBack()
+// })
+
+export default router
--- apps/app-house-keeper/ios/Pods/Target Support Files/Pods-fruit-shop-ios/Pods-fruit-shop-ios-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_fruit_shop_ios : NSObject
-@end
-@implementation PodsDummy_Pods_fruit_shop_ios
-@end

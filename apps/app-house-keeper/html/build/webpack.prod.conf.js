d346400757db280f871fd31aafd6037e6c11b889
michael <767709667@qq.com>
2017-10-11 17:01:55 +0800

    [hybird] copy bundle to ios hybird assets

--- apps/app-house-keeper/html/build/webpack.prod.conf.js
+++ apps/app-house-keeper/html/build/webpack.prod.conf.js
@@ -105,7 +105,7 @@ var webpackConfig = merge(baseWebpackConfig, {
             input: config.build.assetsRoot,
             output: [
                 path.resolve(config.build.assetsRoot, '../../android/arsenal/modules/module-housekeeper-hybird/src/main/assets/bundle.zip'),
-                path.resolve(config.build.assetsRoot, '../../ios/housekeeper/assets/bundle.zip')
+                path.resolve(config.build.assetsRoot, '../../ios/arsenal/modules/module-housekeeper-hybird/assets/bundle.zip')
             ]
         })
     ]

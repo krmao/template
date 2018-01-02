15585b70b72026416143ecc0b46ddf8289186306
maokangren <767709667@qq.com>
2018-01-02 20:56:45 +0800

    [hybird] 基础库代码合并整理之前后台切换BUG修复

--- apps/app-house-keeper/android/arsenal/libraries/library-base/src/main/java/com/smart/library/base/HKActivityLifecycleCallbacks.kt
+++ apps/app-house-keeper/android/arsenal/libraries/library-base/src/main/java/com/smart/library/base/HKActivityLifecycleCallbacks.kt
@@ -7,7 +7,7 @@ import com.smart.library.util.HKLogUtil
 import com.smart.library.util.rx.RxBus
 import kotlin.properties.Delegates
 
-@Suppress("unused", "MemberVisibilityCanPrivate")
+@Suppress("unused", "MemberVisibilityCanPrivate", "UNUSED_ANONYMOUS_PARAMETER")
 open class HKActivityLifecycleCallbacks : Application.ActivityLifecycleCallbacks {
     // I use four separate variables here. You can, of course, just use two and
     // increment/decrement them instead of using four and incrementing them all.
@@ -22,41 +22,52 @@ open class HKActivityLifecycleCallbacks : Application.ActivityLifecycleCallbacks
 
     override fun onActivityResumed(activity: Activity) {
         ++resumed
-        ++applicationVisibleChanged
+        HKLogUtil.w("applicationLifeCycle", "application is in foreground: " + (resumed > paused))
     }
 
     override fun onActivityPaused(activity: Activity) {
         ++paused
-        --applicationVisibleChanged
-        HKLogUtil.w("test", "application is in foreground: " + (resumed > paused))
+        HKLogUtil.w("applicationLifeCycle", "application is in foreground: " + (resumed > paused))
     }
 
     override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle?) = Unit
 
     override fun onActivityStarted(activity: Activity) {
         ++started
+        HKLogUtil.w("applicationLifeCycle", "application is visible " + (resumed > paused))
     }
 
     override fun onActivityStopped(activity: Activity) {
         ++stopped
-        HKLogUtil.w("test", "application is visible: " + (started > stopped))
+        HKLogUtil.w("applicationLifeCycle", "application is visible: " + (started > stopped))
     }
 
     companion object {
 
-        private var resumed = 0
-        private var paused = 0
-        private var started = 0
-        private var stopped = 0
-
-        @Suppress("UNUSED_ANONYMOUS_PARAMETER")
-        private var applicationVisibleChanged by Delegates.observable(0) { property, oldValue, newValue ->
-            if (oldValue != newValue)
-                RxBus.post(ApplicationVisibleChangedEvent(isApplicationVisible))
+        private var resumed: Int by Delegates.observable(0) { _, _, newValue ->
         }
 
-        val isApplicationVisible: Boolean
-            get() = applicationVisibleChanged > 0
+        private var paused: Int by Delegates.observable(0) { _, _, newValue ->
+        }
+
+        private var started: Int by Delegates.observable(0) { _, _, newValue ->
+            isApplicationVisible = newValue > stopped
+        }
+
+        private var stopped: Int by Delegates.observable(0) { _, _, newValue ->
+            isApplicationVisible = started > newValue
+        }
+
+        @Suppress("UNUSED_ANONYMOUS_PARAMETER")
+        var isApplicationVisible: Boolean by Delegates.observable(false) { property, oldValue, newValue ->
+            HKLogUtil.w("applicationLifeCycle", "(isApplicationVisible : oldValue=$oldValue , newValue=$newValue)")
+            HKLogUtil.w("applicationLifeCycle", "(resumed=$resumed , paused=$paused)")
+            HKLogUtil.w("applicationLifeCycle", "(started=$started , stopped=$stopped)")
+            if (oldValue != newValue) {
+                HKLogUtil.e("applicationLifeCycle", "系统监测到应用程序 正在 从 ${if (oldValue) "前台" else "后台"} 切换到 ${if (newValue) "前台" else "后台"} ")
+                RxBus.post(ApplicationVisibleChangedEvent(newValue))
+            }
+        }
     }
 
     class ApplicationVisibleChangedEvent(val isApplicationVisible: Boolean)

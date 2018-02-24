047a0eb92c2a945663c50b31e6925f67a225289e
maokangren <767709667@qq.com>
2018-02-24 17:44:07 +0800

    [react native]flex Z 轴

--- apps/app-template/reactnative/home/HomeComponent.js
+++ apps/app-template/reactnative/home/HomeComponent.js
@@ -1,16 +1,11 @@
 import React, {PureComponent} from "react";
-import {FlatList, RefreshControl, Text, TouchableOpacity, View} from "react-native";
-
-var Dimensions = require('Dimensions');
-
-var SCREEN_WIDTH = Dimensions.get('window').width;
-var SCREEN_HEIGHT = Dimensions.get('window').height;
-var SCREEN_SCALE = Dimensions.get('window').scale;
+import {FlatList, RefreshControl, Text, TouchableHighlight, TouchableOpacity, View} from "react-native";
+import CXSystemUtil from "../core/CXSystemUtil";
 
 function getDataList(count: int) {
     let dataList = [];
     for (var i = 0; i < count; i++) {
-        dataList.push({title: "hello michael mao nice to meet you now, hello michael mao nice to meet you now !!! --> "})
+        dataList.push({title: "hello react native, nice to meet you now, I am very happy to make friends with you !!! --> "})
     }
     return dataList;
 }
@@ -60,45 +55,13 @@ export default class HomeComponent extends React.PureComponent {
                 }, 2000);
             }
         };
-    }
 
-    render() {
-        return (
-            <FlatList
-                data={this.state.dataList}
-                numColumns={1}
-                onEndReached={this.onLoadMore}
-
-                ItemSeparatorComponent={() =>
-                    <View style={{height: 1, backgroundColor: '#EEEEEE'}}/>
-                }
-
-                renderItem={({item, index}) => (
-                    <ItemView
-                        item={item}
-                        index={index}
-                        currentIndex={this.state.currentIndex}
-                        onPressItem={(index: string) => {
-                            this.setState({currentIndex: index});
-                        }}
-                    />
-                )}
-
-                refreshControl={
-                    <RefreshControl
-                        refreshing={this.state.isRefreshingNow}
-                        onRefresh={this.onRefresh}
-                        title="Loading..."
-                        titleColor="#00ff00"
-                        tintColor="#ff0000"
-                        colors={['#ff0000', '#00ff00', '#0000ff']}
-                        progressBackgroundColor="#ffffff"
-                    />
-                }
-
-                ListHeaderComponent={ <View
+        this.headerView = () => {
+            return (
+                <View
                     style={{
-                        height: 48,
+                        paddingTop: CXSystemUtil.STATES_BAR_HEIGHT,
+                        height: 100 + CXSystemUtil.STATES_BAR_HEIGHT,
                         justifyContent: 'center',
                         backgroundColor: 'red',
                         alignItems: 'center'
@@ -107,75 +70,145 @@ export default class HomeComponent extends React.PureComponent {
                 >
                     <Text
                         style={{
-                            fontSize: 24,
+                            fontSize: 30,
                             fontWeight: 'bold',
                             fontStyle: 'italic',
-                            textAlign: 'center',
+                            textAlign: 'center'
                         }}
                     >
                         {'HEADER VIEW'}
                     </Text>
                 </View>
-                }
-                ListFooterComponent={() => {
-                    if (this.state.dataList.length !== 0 && this.state.loadMoreState === 'loading') {
-                        return (
-                            <View
-                                style={{
-                                    height: 100,
-                                    justifyContent: 'center',
-                                    backgroundColor: 'red',
-                                    alignItems: 'center'
-                                }}
-                                activeOpacity={1}
-                            >
-                                <Text
-                                    style={{
-                                        fontSize: 24,
-                                        fontWeight: 'bold',
-                                        fontStyle: 'italic',
-                                        textAlign: 'center',
-                                    }}
-                                >
-                                    {'努力加载中...'}
-                                </Text>
-                            </View>
-                        )
-                    } else if (this.state.loadMoreState === 'empty') {
-                        return (
-                            <View
-                                style={{
-                                    height: 100,
-                                    justifyContent: 'center',
-                                    backgroundColor: 'red',
-                                    alignItems: 'center'
-                                }}
-                                activeOpacity={1}
-                            >
-                                <Text
-                                    style={{
-                                        fontSize: 24,
-                                        fontWeight: 'bold',
-                                        fontStyle: 'italic',
-                                        textAlign: 'center',
-                                    }}
-                                >
-                                    {'没有更多数据了...'}
-                                </Text>
-                            </View>
-                        )
-                    } else {
-                        return null
-                    }
-                }}
+            )
+        };
 
-                extraData={this.state}                        // 当指定的值变化时,会触发 FlatList 更新
-                keyExtractor={(item, index) => index}         // 使用 index 作为区分不同元素的 key
-                enableEmptySections={true}
-                onEndReachedThreshold={0.1}                   // 执行上啦的时候10%执行
-                showsVerticalScrollIndicator={true}           // 是否显示垂直滚动条
-                showsHorizontalScrollIndicator={false}        // 是否显示水平滚动条
-            />
+        this.loadMoreView = () => {
+            if (this.state.dataList.length !== 0 && this.state.loadMoreState === 'loading') {
+                return (
+                    <View
+                        style={{
+                            height: 48,
+                            justifyContent: 'center',
+                            backgroundColor: '#EEEEEE',
+                            alignItems: 'center'
+                        }}
+                        activeOpacity={1}
+                    >
+                        <Text
+                            style={{
+                                color: '#333',
+                                fontSize: 12,
+                                fontWeight: 'bold',
+                                fontStyle: 'italic',
+                                textAlign: 'center',
+                            }}
+                        >
+                            {'努力加载中...'}
+                        </Text>
+                    </View>
+                )
+            } else if (this.state.loadMoreState === 'empty') {
+                return (
+                    <View
+                        style={{
+                            height: 48,
+                            justifyContent: 'center',
+                            backgroundColor: '#EEEEEE',
+                            alignItems: 'center'
+                        }}
+                        activeOpacity={1}
+                    >
+                        <Text
+                            style={{
+                                color: '#333',
+                                fontSize: 12,
+                                fontWeight: 'bold',
+                                fontStyle: 'italic',
+                                textAlign: 'center',
+                            }}
+                        >
+                            {'没有更多数据了...'}
+                        </Text>
+                    </View>
+                )
+            } else {
+                return null
+            }
+        }
+    }
+
+    render() {
+        return (
+            <View style={{
+                flex: 1,
+            }}>
+
+                <FlatList
+                    style={{backgroundColor: 'yellow'}}
+                    data={this.state.dataList}
+                    numColumns={1}
+                    onEndReached={this.onLoadMore}
+
+                    ItemSeparatorComponent={() =>
+                        <View style={{height: 1, backgroundColor: '#EEEEEE'}}/>
+                    }
+
+                    renderItem={({item, index}) => (
+                        <ItemView
+                            item={item}
+                            index={index}
+                            currentIndex={this.state.currentIndex}
+                            onPressItem={(index: string) => {
+                                this.setState({currentIndex: index});
+                            }}
+                        />
+                    )}
+
+                    refreshControl={
+                        <RefreshControl
+                            refreshing={this.state.isRefreshingNow}
+                            onRefresh={this.onRefresh}
+                            title="Loading..."
+                            titleColor="#00ff00"
+                            tintColor="#ff0000"
+                            colors={['#ff0000', '#00ff00', '#0000ff']}
+                            progressBackgroundColor="#ffffff"
+                        />
+                    }
+
+                    ListHeaderComponent={this.headerView }
+                    ListFooterComponent={this.loadMoreView}
+                    extraData={this.state}                        // 当指定的值变化时,会触发 FlatList 更新
+                    keyExtractor={(item, index) => index}         // 使用 index 作为区分不同元素的 key
+                    enableEmptySections={true}
+                    onEndReachedThreshold={0.1}                   // 距离底部还剩下 屏幕高度10% 的时候,触发 loadMore
+                    showsVerticalScrollIndicator={true}           // 是否显示垂直滚动条
+                    showsHorizontalScrollIndicator={false}        // 是否显示水平滚动条
+                />
+
+                <View
+                    style={{flex: 1, top: -60, alignSelf: 'flex-end', justifyContent: 'center', alignItems: 'center'}}>
+                    <TouchableHighlight
+                        style={{
+                            backgroundColor: 'blue',
+                            width: CXSystemUtil.SCREEN_WIDTH * 0.1,
+                            height: CXSystemUtil.SCREEN_WIDTH * 0.1,
+                            borderRadius: CXSystemUtil.SCREEN_WIDTH * 0.1,
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            margin: 30
+                        }}
+                        underlayColor={'pink'}
+                        onPress={() => {
+
+                        }}
+                    >
+                        <Text style={{color: 'white', fontWeight: 'bold', fontSize: 24}}>
+                            +
+                        </Text>
+                    </TouchableHighlight>
+                </View>
+            </View>
         );
     }
 }
@@ -184,15 +217,16 @@ class ItemView extends React.PureComponent {
     render() {
         return (
             <TouchableOpacity onPress={() => this.props.onPressItem(this.props.index)}>
-                <View style={{height: SCREEN_HEIGHT / 3}}>
+                <View style={{
+                    height: CXSystemUtil.SCREEN_HEIGHT / 3,
+                    justifyContent: 'center',
+                    backgroundColor: '#0033aa88'
+                }}>
                     <Text
                         style={{
-                            flex: 1,
+                            margin: 15,
+                            padding: 15,
                             color: this.props.currentIndex === this.props.index ? "black" : "white",
-                            paddingTop: 80,
-                            paddingBottom: 80,
-                            paddingLeft: 15,
-                            paddingRight: 15,
                             fontSize: 24,
                             fontWeight: 'bold',
                             fontStyle: 'italic',

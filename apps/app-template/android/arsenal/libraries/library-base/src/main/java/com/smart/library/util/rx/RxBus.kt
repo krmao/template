628f1a4e6e1b126fe034066173f8ec4a783fa01c
maokangren <767709667@qq.com>
2018-03-02 08:20:08 +0800

    [android] 优化RxBus

--- apps/app-template/android/arsenal/libraries/library-base/src/main/java/com/smart/library/util/rx/RxBus.kt
+++ apps/app-template/android/arsenal/libraries/library-base/src/main/java/com/smart/library/util/rx/RxBus.kt
@@ -1,20 +1,78 @@
 package com.smart.library.util.rx
 
-/*
+import java.util.concurrent.ConcurrentHashMap
 
-RxBus.toObservable(HKTestingFragment.ChangeEvent::class.java).subscribe { changeEvent ->
-    try {
-        CXURLManager.curEnvironment = CXURLManager.Environments.valueOf(changeEvent.model.name)
-    } catch (_: Exception) {
-    }
-    CXToastUtil.show("检测到环境切换(${changeEvent.model.name})\n已切换到:${CXURLManager.curEnvironment.name}")
-}
-
+/**
+ * 使用RxBus 注意取消订阅, 避免内存泄露
+ *
+ * 案例:
+ *      //任意的 RxJava 的订阅都应该如此操作
+ *      val compositeDisposable = CompositeDisposable()
+ *
+ *      compositeDisposable.add(RxBus.toObservable(CXApplicationVisibleChangedEvent::class.java).subscribe { changeEvent ->
+ *          if (changeEvent.isApplicationVisible)
+ *              CXDebugFragment.showDebugNotification(notificationId)
+ *          else
+ *              CXDebugFragment.cancelDebugNotification(notificationId)
+ *      })
+ *
+ *      //当前页面退出的时候调用 比如 fragment/activity 的 onDestroy 方法里面调用
+ *      compositeDisposable.dispose()
  */
+@Suppress("unused")
 object RxBus {
+
+    //==============================================================================================
+    // 标准广播事件 START
+    //==============================================================================================
+
     private val bus: io.reactivex.subjects.PublishSubject<Any> by lazy { io.reactivex.subjects.PublishSubject.create<Any>() }
 
-    fun post(event: Any) = bus.onNext(event)
 
-    fun <T> toObservable(eventType: Class<T>): io.reactivex.Observable<T> = bus.ofType(eventType)
+    fun post(event: Any) {
+        bus.onNext(event)
+    }
+
+    fun <T> toObservable(eventType: Class<T>): io.reactivex.Observable<T> {
+        return bus.ofType(eventType)
+    }
+
+    //==============================================================================================
+    // 标准广播事件 END and 粘性广播事件 START
+    //==============================================================================================
+
+    private val stickyEventMap: ConcurrentHashMap<Class<*>, Any> by lazy { ConcurrentHashMap<Class<*>, Any>() }
+
+    /**
+     * 发送粘性广播, A 订阅者收到10条数据后 B 才开始订阅, B也会收到全部10条数据
+     * 注意不用的时候 调用 removeStickyEvent 清除历史事件
+     * 在程序退出的时候 调用 removeAllStickyEvents 清除所有历史事件
+     */
+    fun postSticky(event: Any) {
+        stickyEventMap[event.javaClass] = event
+        post(event)
+    }
+
+    fun <T> toObservableSticky(eventType: Class<T>): io.reactivex.Observable<T> {
+        val observable = bus.ofType(eventType)
+        val event = stickyEventMap[eventType]
+        return if (event != null) observable.mergeWith(io.reactivex.Observable.just(eventType.cast(event))) else observable
+    }
+
+    fun <T> getStickyEvent(eventType: Class<T>): T {
+        return eventType.cast(stickyEventMap[eventType])
+    }
+
+    fun <T> removeStickyEvent(eventType: Class<T>): T {
+        return eventType.cast(stickyEventMap.remove(eventType))
+    }
+
+    fun removeAllStickyEvents() {
+        stickyEventMap.clear()
+    }
+
+    //==============================================================================================
+    // 粘性广播事件 END
+    //==============================================================================================
+
 }

7111c9285931a0dd195be1c4d427a2a4603ae7ab
maokangren <767709667@qq.com>
2018-03-18 17:42:10 +0800

    点击切换 当前选中颜色

---
+++ services/service-template/arsenal/apps/app-template/src/main/kotlin/com/smart/template/http/controller/DeveloperController.kt
@@ -0,0 +1,42 @@
+package com.smart.template.http.controller
+
+import com.smart.template.base.config.config.CXConfig
+import com.smart.template.base.util.CXChecksumUtil
+import com.smart.template.base.util.CXFileUtil
+import com.smart.template.http.model.HKResponse
+import io.swagger.annotations.ApiOperation
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestPart
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.multipart.MultipartFile
+import java.io.File
+
+@RestController
+@RequestMapping("/developer")
+class DeveloperController() {
+    private val log: Logger = LogManager.getLogger(DeveloperController::class.java.name)
+
+    @ApiOperation("上传文件", notes = "返回相对路径")
+    @PostMapping("/upload")
+    fun upload(@RequestPart("files") files: Array<MultipartFile>): HKResponse<Any> {
+        if (files.count() == 0) return HKResponse.ok()
+
+        println("filesDir=${CXConfig.DEFAULT_FILES_DIR.absolutePath}")
+        println("filesDir.exists=${CXConfig.DEFAULT_FILES_DIR.exists()}")
+
+        val filePathMap = mutableMapOf<String, String>()
+
+        for (file in files) {
+            val oldName = file.originalFilename
+            val newName = CXChecksumUtil.genMD5Checksum(oldName + "-" + System.currentTimeMillis())
+            CXFileUtil.copy(file.inputStream, File(CXConfig.DEFAULT_FILES_DIR, newName))
+            filePathMap.put(oldName, newName)
+        }
+
+        log.warn("filePathMap:" + filePathMap)
+        return HKResponse(filePathMap)
+    }
+}

ab3d9759154f479e8ec90ef7e9580f6d1af6e2a0
maokangren <767709667@qq.com>
2018-06-22 16:38:10 +0800

    [android rn] custom load

--- apps/app-template/android/arsenal/modules/module-template-react_native/src/main/java/com/smart/template/module/rn/ReactConstant.kt
+++ apps/app-template/android/arsenal/modules/module-template-react_native/src/main/java/com/smart/template/module/rn/ReactConstant.kt
@@ -1,9 +1,13 @@
 package com.smart.template.module.rn
 
 object ReactConstant {
+//
+//    const val PATH_ASSETS_BASE_BUNDLE = "assets://base.bundle"
+//    const val PATH_ASSETS_BUSINESS_BUNDLE = "assets://business.bundle"
+//    const val COMPONENT_NAME = "Page1"
 
-    const val PATH_ASSETS_BASE_BUNDLE = "assets://base.bundle"
-    const val PATH_ASSETS_BUSINESS_BUNDLE = "assets://business.bundle"
-    const val COMPONENT_NAME = "Page1"
+    const val PATH_ASSETS_BASE_BUNDLE = "assets://base.android.bundle"
+    const val PATH_ASSETS_BUSINESS_BUNDLE = "assets://business.android.bundle"
+    const val COMPONENT_NAME = "cxj"
 
 }

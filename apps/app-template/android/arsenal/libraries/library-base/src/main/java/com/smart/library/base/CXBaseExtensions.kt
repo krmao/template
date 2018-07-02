109b8870027689caa7e60bdb114eb9c2732f4bb5
maokangren <767709667@qq.com>
2018-07-02 21:16:58 +0800

    [android] kotlin extension

--- apps/app-template/android/arsenal/libraries/library-base/src/main/java/com/smart/library/base/CXBaseExtensions.kt
+++ apps/app-template/android/arsenal/libraries/library-base/src/main/java/com/smart/library/base/CXBaseExtensions.kt
@@ -2,17 +2,26 @@
 
 package com.smart.library.base
 
+import android.animation.Animator
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.drawable.Drawable
+import android.os.Bundle
+import android.support.annotation.RequiresPermission
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentActivity
 import android.text.TextUtils
 import android.view.View
 import android.widget.AbsListView
 import android.widget.TextView
 import com.smart.library.util.CXChecksumUtil
+import com.smart.library.util.CXValueUtil
 import com.smart.library.util.CXViewUtil
 import org.jetbrains.anko.AnkoAsyncContext
+import java.util.concurrent.ConcurrentHashMap
 
 /**
  * 全局基础扩展属性/方法
@@ -55,3 +64,113 @@ fun TextView.setTextAndVisible(text: String?) {
     this.visibility = if (TextUtils.isEmpty(text?.trim())) View.GONE else View.VISIBLE
 }
 fun String.md5(): String = CXChecksumUtil.genMD5Checksum(this)
+
+
+fun Fragment.uiThread(fn: () -> Unit) {
+    if (this.isDetached) return
+    val activity = this.activity ?: return
+    activity.runOnUiThread { fn() }
+}
+
+fun View.animateAlphaToVisibility(visibility: Int, duration: Long = 300) {
+    animate().alpha(if (visibility == View.VISIBLE) 1.0f else 0.0f).setDuration(duration).setListener(object : Animator.AnimatorListener {
+        override fun onAnimationRepeat(animation: Animator?) {
+            setVisibility(View.VISIBLE)
+        }
+
+        override fun onAnimationEnd(animation: Animator?) {
+            setVisibility(visibility)
+        }
+
+        override fun onAnimationCancel(animation: Animator?) {
+            setVisibility(visibility)
+        }
+
+        override fun onAnimationStart(animation: Animator?) {
+            setVisibility(View.VISIBLE)
+        }
+    }).start()
+}
+
+
+// startActivityForResult with call back -->
+
+@JvmOverloads
+fun startActivityForResult(activity: Activity?, @RequiresPermission intent: Intent, requestCode: Int, options: Bundle? = null, callback: ((requestCode: Int, resultCode: Int, data: Intent?) -> Unit?)? = null) {
+    activity?.let {
+        if (CXValueUtil.isValid(activity)) {
+            if (activity is FragmentActivity) {
+                activity.callbackFragmentV4?.startForResult(intent, requestCode, options, callback)
+            } else {
+                activity.callbackFragment?.startForResult(intent, requestCode, options, callback)
+            }
+        }
+    }
+}
+
+private val Activity.callbackFragment: ActivityCallbackFragment?
+    get() {
+        return (fragmentManager?.findFragmentByTag(ActivityCallbackFragment.TAG) as ActivityCallbackFragment?) ?: ActivityCallbackFragment().apply {
+            fragmentManager?.let {
+                it.beginTransaction().add(this, ActivityCallbackFragment.TAG).commitAllowingStateLoss()
+                it.executePendingTransactions()
+            }
+        }
+    }
+private val FragmentActivity.callbackFragmentV4: ActivityCallbackFragmentV4?
+    get() {
+        return (supportFragmentManager?.findFragmentByTag(ActivityCallbackFragment.TAG) as ActivityCallbackFragmentV4?) ?: ActivityCallbackFragmentV4().apply {
+            supportFragmentManager?.let {
+                it.beginTransaction().add(this, ActivityCallbackFragment.TAG).commitAllowingStateLoss()
+                it.executePendingTransactions()
+            }
+        }
+    }
+
+internal class ActivityCallbackFragmentV4 : android.support.v4.app.Fragment() {
+    companion object {
+        const val TAG: String = "ActivityCallbackFragment"
+    }
+
+    private val callbackMap: MutableMap<Int, ((requestCode: Int, resultCode: Int, data: Intent?) -> Unit?)?> = ConcurrentHashMap()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        retainInstance = true
+    }
+
+    fun startForResult(@RequiresPermission intent: Intent, requestCode: Int, options: Bundle?, callback: ((requestCode: Int, resultCode: Int, data: Intent?) -> Unit?)? = null) {
+        callbackMap[requestCode] = callback
+        startActivityForResult(intent, requestCode, options)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        callbackMap.remove(requestCode)?.invoke(requestCode, resultCode, data)
+    }
+}
+
+internal class ActivityCallbackFragment : android.app.Fragment() {
+    companion object {
+        const val TAG: String = "ActivityCallbackFragment"
+    }
+
+    private val callbackMap: MutableMap<Int, ((requestCode: Int, resultCode: Int, data: Intent?) -> Unit?)?> = ConcurrentHashMap()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        retainInstance = true
+    }
+
+    fun startForResult(@RequiresPermission intent: Intent, requestCode: Int, options: Bundle?, callback: ((requestCode: Int, resultCode: Int, data: Intent?) -> Unit?)? = null) {
+        callbackMap[requestCode] = callback
+        startActivityForResult(intent, requestCode, options)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        callbackMap.remove(requestCode)?.invoke(requestCode, resultCode, data)
+    }
+}
+
+// startActivityForResult with call back <--

9ca031fcd53e1c9cd6cdfd37ea67052e8a5ed4ed
maokangren <767709667@qq.com>
2018-11-15 17:54:47 +0800

    [flutter] dialog with back pressed

--- apps/app-template/flutter_smart/lib/com/smart/business/BPage.dart
+++ apps/app-template/flutter_smart/lib/com/smart/business/BPage.dart
@@ -22,8 +22,49 @@ class BPageState extends DefaultPageState<BPage> {
   @override
   Widget buildBody() {
     return getOnTapWidget(Container(color: Colors.yellow), () {
-      NativeManager.goTo(CPage());
+      _showDialog();
     });
   }
 
+  @override
+  bool onBackPressed() {
+    if (isShow) {
+      Navigator.of(context).pop();
+      isShow = false;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  bool isShow = false;
+
+  Future<Null> _showDialog() async {
+    isShow = true;
+    return showDialog<Null>(
+      context: context,
+      barrierDismissible: true, // user must tap button!
+      builder: (BuildContext context) {
+        return new AlertDialog(
+          title: new Text('弹框'),
+          content: new Text('弹框'),
+          actions: <Widget>[
+            new FlatButton(
+              child: new Text('取消'),
+              onPressed: () {
+                Navigator.of(context).pop();
+                isShow = false;
+              },
+            ),
+            new FlatButton(
+              child: new Text('下一页'),
+              onPressed: () {
+                NativeManager.goTo(CPage());
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 }

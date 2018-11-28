7ba94ff0fa74206c4550c88480037101f0cf725b
maokangren <767709667@qq.com>
2018-11-28 11:00:56 +0800

    mpvue add new page

--- apps/app-template/wechat_mpvue/src/pages/blog/index.vue
+++ apps/app-template/wechat_mpvue/src/pages/blog/index.vue
@@ -13,30 +13,30 @@
     methods: {},
 
     // vue 生命周期 ========================================================================================================
-    beforeCreate () { console.log('vue [page-index] beforeCreate') },
-    created () { console.log('vue [page-index] created') },
-    beforeMount () { console.log('vue [page-index] beforeMount') },
-    mounted () { console.log('vue [page-index] mounted') },
-    beforeUpdate () { console.log('vue [page-index] beforeUpdate') },
-    updated () { console.log('vue [page-index] updated') },
-    activated () { console.log('vue [page-index] activated, mpvue 实际运行并没有回调, 原因不支持 keep-alive') },
-    deactivated () { console.log('vue [page-index] deactivated, mpvue 实际运行并没有回调, 原因不支持 keep-alive') },
-    beforeDestroy () { console.log('vue [page-index] beforeDestroy, mpvue 实际运行并没有回调, bug') },
-    destroyed () { console.log('vue [page-index] destroyed, mpvue 实际运行并没有回调, bug') },
+    beforeCreate () { console.log('vue [page-blog] beforeCreate') },
+    created () { console.log('vue [page-blog] created') },
+    beforeMount () { console.log('vue [page-blog] beforeMount') },
+    mounted () { console.log('vue [page-blog] mounted') },
+    beforeUpdate () { console.log('vue [page-blog] beforeUpdate') },
+    updated () { console.log('vue [page-blog] updated') },
+    activated () { console.log('vue [page-blog] activated, mpvue 实际运行并没有回调, 原因不支持 keep-alive') },
+    deactivated () { console.log('vue [page-blog] deactivated, mpvue 实际运行并没有回调, 原因不支持 keep-alive') },
+    beforeDestroy () { console.log('vue [page-blog] beforeDestroy, mpvue 实际运行并没有回调, bug') },
+    destroyed () { console.log('vue [page-blog] destroyed, mpvue 实际运行并没有回调, bug') },
     // vue 生命周期 ========================================================================================================
 
     // 小程序自有生命周期 ====================================================================================================
-    onLoad () { console.log('小程序 [page-index] 生命周期: onLoad: 监听页面加载') },
-    onShow () { console.log('小程序 [page-index] 生命周期: onShow: 监听页面显示') },
-    onReady () { console.log('小程序 [page-index] 生命周期: onReady: 监听页面初次渲染完成') },
-    onHide () { console.log('小程序 [page-index] 生命周期: onHide: 监听页面隐藏') },
-    onUnload () { console.log('小程序 [page-index] 生命周期: onUnload: 监听页面卸载') },
-    onPullDownRefresh () { console.log('小程序 [page-index] 生命周期: onPullDownRefresh: 监听用户下拉动作') },
-    onReachBottom () { console.log('小程序 [page-index] 生命周期: onReachBottom: 页面上拉触底事件的处理函数') },
-    onShareAppMessage () { console.log('小程序 [page-index] 生命周期: onShareAppMessage: 用户点击右上角分享') },
-    onPageScroll () { console.log('小程序 [page-index] 生命周期: onPageScroll: 页面滚动') },
-    onResize () { console.log('小程序 [page-index] 生命周期: onResize: 宽高改变') },
-    onTabItemTap (item) { console.log('小程序 [page-index] 生命周期: onTabItemTap: 当前是 tab 页时，点击 tab 时触发 （mpvue 0.0.16 支持）') }
+    onLoad () { console.log('小程序 [page-blog] 生命周期: onLoad: 监听页面加载') },
+    onShow () { console.log('小程序 [page-blog] 生命周期: onShow: 监听页面显示') },
+    onReady () { console.log('小程序 [page-blog] 生命周期: onReady: 监听页面初次渲染完成') },
+    onHide () { console.log('小程序 [page-blog] 生命周期: onHide: 监听页面隐藏') },
+    onUnload () { console.log('小程序 [page-blog] 生命周期: onUnload: 监听页面卸载') },
+    onPullDownRefresh () { console.log('小程序 [page-blog] 生命周期: onPullDownRefresh: 监听用户下拉动作') },
+    onReachBottom () { console.log('小程序 [page-blog] 生命周期: onReachBottom: 页面上拉触底事件的处理函数') },
+    onShareAppMessage () { console.log('小程序 [page-blog] 生命周期: onShareAppMessage: 用户点击右上角分享') },
+    onPageScroll () { console.log('小程序 [page-blog] 生命周期: onPageScroll: 页面滚动') },
+    onResize () { console.log('小程序 [page-blog] 生命周期: onResize: 宽高改变') },
+    onTabItemTap (item) { console.log('小程序 [page-blog] 生命周期: onTabItemTap: 当前是 tab 页时，点击 tab 时触发 （mpvue 0.0.16 支持）') }
     // 小程序自有生命周期 ====================================================================================================
   }
 </script>

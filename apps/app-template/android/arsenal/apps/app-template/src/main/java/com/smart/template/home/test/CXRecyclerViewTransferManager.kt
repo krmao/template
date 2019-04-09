199ae0cafee818a6cb78ebf0445158f4a7c190ee
kr.mao <kr.mao@ctrip.com>
2019-04-09 12:23:05 +0800

    拖拽 recyclerView 的 item 到另一个 recyclerView中

---
+++ apps/app-template/android/arsenal/apps/app-template/src/main/java/com/smart/template/home/test/CXRecyclerViewTransferManager.kt
@@ -0,0 +1,133 @@
+package com.smart.template.home.test
+
+import android.content.ClipData
+import android.support.v7.widget.RecyclerView
+import android.view.DragEvent
+import android.view.View
+import android.widget.TextView
+import com.smart.library.util.CXLogUtil
+import com.smart.template.R
+
+/**
+ * 将一个 recyclerView 中的 item 拖拽到 另一个 recyclerView 中
+ *
+ * @param fromRecyclerView 待拖拽的 RecyclerView
+ * @param toRecyclerView   拖拽到的 目标RecyclerView
+ * @param onFromRecyclerViewEnding 在 fromRecyclerView 中执行 删除等操作
+ * @param onToRecyclerViewDropping 在 toRecyclerView   中执行 插入等操作
+ */
+@Suppress("DEPRECATION", "MemberVisibilityCanBePrivate", "PrivatePropertyName", "UNUSED_ANONYMOUS_PARAMETER", "unused")
+class CXRecyclerViewTransferManager(val fromRecyclerView: RecyclerView, val toRecyclerView: RecyclerView, val onFromRecyclerViewEnding: (fromRecyclerView: RecyclerView, fromPosition: Int) -> Unit, val onToRecyclerViewDropping: (toRecyclerView: RecyclerView, fromPosition: Int, toPosition: Int) -> Unit) {
+
+    private val TAG = "recyclerView transfer"
+    private var backgroundView: View? = null
+    private var dataOriginPosition: Int = -1
+    private var dataNewPosition: Int = -1 // -1 默认值, -2 目标recyclerView 已添加, 则原始目标recyclerView 需要删除对应项
+
+    init {
+        toRecyclerView.setOnDragListener { dragView, dragEvent ->
+
+            // 松手时在 原始 recyclerView 边界之内 ACTION_DRAG_STARTED -> ACTION_DRAG_ENTERED -> ACTION_DROP        -> ACTION_DRAG_ENDED
+            // 松手时在 原始 recyclerView 边界之外 ACTION_DRAG_STARTED -> ACTION_DRAG_ENTERED -> ACTION_DRAG_EXITED -> ACTION_DRAG_ENDED
+            when (dragEvent.action) {
+                // 第一次长按开始 drag 会触发
+                DragEvent.ACTION_DRAG_STARTED -> CXLogUtil.w(TAG, "days ACTION_DRAG_STARTED")
+                // 进入 原始 recyclerView 边界会触发, 第一次长按开始 drag 也会触发
+                DragEvent.ACTION_DRAG_ENTERED -> CXLogUtil.w(TAG, "days ACTION_DRAG_ENTERED")
+                // 离开 原始 recyclerView 边界会触发
+                DragEvent.ACTION_DRAG_EXITED -> {
+                    CXLogUtil.w(TAG, "days ACTION_DRAG_EXITED")
+                    backgroundView?.setBackgroundColor(toRecyclerView.resources.getColor(R.color.orange_700))
+                    dataNewPosition = -1
+                }
+
+                // 长按后移动会连续触发
+                DragEvent.ACTION_DRAG_LOCATION -> {
+                    val onTopOfView = toRecyclerView.findChildViewUnder(dragEvent.x, dragEvent.y)
+                    onTopOfView?.let {
+                        dataNewPosition = toRecyclerView.getChildAdapterPosition(it)
+                        backgroundView?.setBackgroundColor(toRecyclerView.resources.getColor(R.color.orange_700))
+                        backgroundView = it.findViewById<TextView>(R.id.textViewDays)
+                        backgroundView?.setBackgroundColor(toRecyclerView.resources.getColor(R.color.blue_700))
+                        Unit
+                    }
+                    CXLogUtil.d(TAG, "days ACTION_DRAG_LOCATION $dataNewPosition")
+                }
+
+                // 松手时如果在 原始 recyclerView 边界内会触发, 边界之外不会触发
+                DragEvent.ACTION_DROP -> {
+                    CXLogUtil.w(TAG, "days ACTION_DROP dataNewPosition=$dataNewPosition, dataOriginPosition=$dataOriginPosition")
+                    backgroundView?.setBackgroundColor(toRecyclerView.resources.getColor(R.color.orange_700))
+
+                    if (dataOriginPosition != -1 && dataNewPosition != -1) {
+                        onToRecyclerViewDropping(toRecyclerView, dataOriginPosition, dataNewPosition)
+                        dataNewPosition = -2 // must be remove
+                    }
+                }
+
+                //松手后最后一个事件, 一定会触发
+                DragEvent.ACTION_DRAG_ENDED -> {
+                    CXLogUtil.w(TAG, "days ACTION_DRAG_ENDED $dataNewPosition")
+                    backgroundView = null
+                }
+                else -> {
+                    CXLogUtil.w(TAG, "days else ${dragEvent.action}")
+                }
+            }
+            true
+        }
+
+        fromRecyclerView.setOnDragListener { dragView, dragEvent ->
+            // val destRecyclerView = dragView as RecyclerView
+            // CXLogUtil.v(TAG, "dayD dragView:${destRecyclerView.tag}")
+            val draggingItemView = dragEvent.localState as View
+            try {
+                dataOriginPosition = fromRecyclerView.getChildAdapterPosition(draggingItemView)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+            // 松手时在 原始 recyclerView 边界之内 ACTION_DRAG_STARTED -> ACTION_DRAG_ENTERED -> ACTION_DROP        -> ACTION_DRAG_ENDED
+            // 松手时在 原始 recyclerView 边界之外 ACTION_DRAG_STARTED -> ACTION_DRAG_ENTERED -> ACTION_DRAG_EXITED -> ACTION_DRAG_ENDED
+            when (dragEvent.action) {
+                // 第一次长按开始 drag 会触发
+                DragEvent.ACTION_DRAG_STARTED -> CXLogUtil.w(TAG, "dayD ACTION_DRAG_STARTED")
+                // 进入 原始 recyclerView 边界会触发, 第一次长按开始 drag 也会触发
+                DragEvent.ACTION_DRAG_ENTERED -> CXLogUtil.w(TAG, "dayD ACTION_DRAG_ENTERED")
+                // 离开 原始 recyclerView 边界会触发
+                DragEvent.ACTION_DRAG_EXITED -> CXLogUtil.w(TAG, "dayD ACTION_DRAG_EXITED")
+                // 长按后移动会连续触发
+                DragEvent.ACTION_DRAG_LOCATION -> CXLogUtil.d(TAG, "dayD ACTION_DRAG_LOCATION")
+
+                // 松手时如果在 原始 recyclerView 边界内会触发, 边界之外不会触发
+                DragEvent.ACTION_DROP -> {
+                    dataOriginPosition = -1
+                    CXLogUtil.w(TAG, "dayD ACTION_DROP dataOriginPosition=$dataOriginPosition")
+                }
+
+                //松手后最后一个事件, 一定会触发
+                DragEvent.ACTION_DRAG_ENDED -> {
+                    CXLogUtil.w(TAG, "dayD ACTION_DRAG_ENDED dataNewPosition=$dataNewPosition, dataOriginPosition=$dataOriginPosition")
+                    if (dataNewPosition == -2 && dataOriginPosition != -1) {
+                        onFromRecyclerViewEnding(fromRecyclerView, dataOriginPosition)
+                        dataNewPosition = -1
+                    } else {
+                        // draggingItemView.visibility = View.VISIBLE
+                    }
+                    dataOriginPosition = -1
+                }
+                else -> {
+                    CXLogUtil.w(TAG, "dayD else ${dragEvent.action}")
+                }
+            }
+            true
+        }
+
+
+    }
+
+    fun startDrag(itemView: View) {
+        itemView.startDrag(ClipData.newPlainText("", ""), View.DragShadowBuilder(itemView), itemView, 0)
+    }
+
+}
--- apps/app-template/android/arsenal/apps/app-template/src/main/java/com/smart/template/home/test/RecyclerViewDragAndTransferFragment.kt
+++ apps/app-template/android/arsenal/apps/app-template/src/main/java/com/smart/template/home/test/RecyclerViewDragAndTransferFragment.kt
@@ -1,13 +1,11 @@
 package com.smart.template.home.test
 
-import android.content.ClipData
 import android.content.Context
 import android.os.Bundle
-import android.view.DragEvent
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import com.smart.library.base.CXActivity
 import com.smart.library.base.CXBaseFragment
 import com.smart.library.util.CXLogUtil
@@ -53,8 +51,8 @@ class RecyclerViewDragAndTransferFragment : CXBaseFragment() {
                 viewHolder.itemView.textViewDayDetail.text = dataList[position]
                 viewHolder.itemView.setOnLongClickListener {
                     CXLogUtil.w(TAG, "start drag ...")
-                    it.startDrag(ClipData.newPlainText("", ""), View.DragShadowBuilder(it), it, 0)
-                    it.visibility = View.INVISIBLE
+                    recyclerViewTransferManager?.startDrag(it)
+                    // it.visibility = View.INVISIBLE remove 时会导致底部空白没有刷新
                     true
                 }
             }
@@ -65,9 +63,7 @@ class RecyclerViewDragAndTransferFragment : CXBaseFragment() {
         return inflater.inflate(R.layout.home_fragment_recycler_view_drag_and_transfer, container, false)
     }
 
-    private var backgroundView: View? = null
-    private var dataOriginPosition: Int = -1
-    private var dataNewPosition: Int = -1 // -1 默认值, -2 目标recyclerView 已添加, 则原始目标recyclerView 需要删除对应项
+    private var recyclerViewTransferManager: CXRecyclerViewTransferManager? = null
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -79,101 +75,11 @@ class RecyclerViewDragAndTransferFragment : CXBaseFragment() {
         recyclerViewDayDetail.adapter = dayDetailAdapter
         recyclerViewDays.tag = "recyclerViewDayDetail"
 
-        recyclerViewDays.setOnDragListener { dragView, dragEvent ->
-
-            // 松手时在 原始 recyclerView 边界之内 ACTION_DRAG_STARTED -> ACTION_DRAG_ENTERED -> ACTION_DROP        -> ACTION_DRAG_ENDED
-            // 松手时在 原始 recyclerView 边界之外 ACTION_DRAG_STARTED -> ACTION_DRAG_ENTERED -> ACTION_DRAG_EXITED -> ACTION_DRAG_ENDED
-            when (dragEvent.action) {
-                // 第一次长按开始 drag 会触发
-                DragEvent.ACTION_DRAG_STARTED -> CXLogUtil.w(TAG, "days ACTION_DRAG_STARTED")
-                // 进入 原始 recyclerView 边界会触发, 第一次长按开始 drag 也会触发
-                DragEvent.ACTION_DRAG_ENTERED -> CXLogUtil.w(TAG, "days ACTION_DRAG_ENTERED")
-                // 离开 原始 recyclerView 边界会触发
-                DragEvent.ACTION_DRAG_EXITED -> {
-                    CXLogUtil.w(TAG, "days ACTION_DRAG_EXITED")
-                    backgroundView?.setBackgroundColor(resources.getColor(R.color.orange_700))
-                    dataNewPosition = -1
-                }
-
-                // 长按后移动会连续触发
-                DragEvent.ACTION_DRAG_LOCATION -> {
-                    val onTopOfView = recyclerViewDays.findChildViewUnder(dragEvent.x, dragEvent.y)
-                    onTopOfView?.let {
-                        dataNewPosition = recyclerViewDays.getChildAdapterPosition(it)
-                        backgroundView?.setBackgroundColor(resources.getColor(R.color.orange_700))
-                        backgroundView = it.findViewById<TextView>(R.id.textViewDays)
-                        backgroundView?.setBackgroundColor(resources.getColor(R.color.blue_700))
-                        Unit
-                    }
-                    CXLogUtil.d(TAG, "days ACTION_DRAG_LOCATION $dataNewPosition , ${daysList.getOrNull(dataNewPosition)}")
-                }
-
-                // 松手时如果在 原始 recyclerView 边界内会触发, 边界之外不会触发
-                DragEvent.ACTION_DROP -> {
-                    CXLogUtil.w(TAG, "days ACTION_DROP dataNewPosition=$dataNewPosition, dataOriginPosition=$dataOriginPosition")
-                    backgroundView?.setBackgroundColor(resources.getColor(R.color.orange_700))
-
-                    if (dataOriginPosition != -1 && dataNewPosition != -1) {
-                        daysAdapter.add(dayDetailList[dataOriginPosition], dataNewPosition)
-                        recyclerViewDays.scrollToPosition(dataNewPosition)
-                        dataNewPosition = -2 // must be remove
-                    }
-                }
-
-                //松手后最后一个事件, 一定会触发
-                DragEvent.ACTION_DRAG_ENDED -> {
-                    CXLogUtil.w(TAG, "days ACTION_DRAG_ENDED $dataNewPosition")
-                    backgroundView = null
-                }
-                else -> {
-                    CXLogUtil.w(TAG, "days else ${dragEvent.action}")
-                }
-            }
-            true
-        }
-
-        recyclerViewDayDetail.setOnDragListener { dragView, dragEvent ->
-            // val destRecyclerView = dragView as RecyclerView
-            // CXLogUtil.v(TAG, "dayD dragView:${destRecyclerView.tag}")
-            val draggingItemView = dragEvent.localState as View
-            try {
-                dataOriginPosition = recyclerViewDayDetail.getChildAdapterPosition(draggingItemView)
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-
-            // 松手时在 原始 recyclerView 边界之内 ACTION_DRAG_STARTED -> ACTION_DRAG_ENTERED -> ACTION_DROP        -> ACTION_DRAG_ENDED
-            // 松手时在 原始 recyclerView 边界之外 ACTION_DRAG_STARTED -> ACTION_DRAG_ENTERED -> ACTION_DRAG_EXITED -> ACTION_DRAG_ENDED
-            when (dragEvent.action) {
-                // 第一次长按开始 drag 会触发
-                DragEvent.ACTION_DRAG_STARTED -> CXLogUtil.w(TAG, "dayD ACTION_DRAG_STARTED")
-                // 进入 原始 recyclerView 边界会触发, 第一次长按开始 drag 也会触发
-                DragEvent.ACTION_DRAG_ENTERED -> CXLogUtil.w(TAG, "dayD ACTION_DRAG_ENTERED")
-                // 离开 原始 recyclerView 边界会触发
-                DragEvent.ACTION_DRAG_EXITED -> CXLogUtil.w(TAG, "dayD ACTION_DRAG_EXITED")
-                // 长按后移动会连续触发
-                DragEvent.ACTION_DRAG_LOCATION -> CXLogUtil.d(TAG, "dayD ACTION_DRAG_LOCATION")
-
-                // 松手时如果在 原始 recyclerView 边界内会触发, 边界之外不会触发
-                DragEvent.ACTION_DROP -> {
-                    dataOriginPosition = -1
-                    CXLogUtil.w(TAG, "dayD ACTION_DROP dataOriginPosition=$dataOriginPosition")
-                }
-
-                //松手后最后一个事件, 一定会触发
-                DragEvent.ACTION_DRAG_ENDED -> {
-                    CXLogUtil.w(TAG, "dayD ACTION_DRAG_ENDED dataNewPosition=$dataNewPosition, dataOriginPosition=$dataOriginPosition")
-                    if (dataNewPosition == -2 && dataOriginPosition != -1) {
-                        dayDetailAdapter.remove(dataOriginPosition)
-                        dataNewPosition = -1
-                    }
-                    dataOriginPosition = -1
-                }
-                else -> {
-                    CXLogUtil.w(TAG, "dayD else ${dragEvent.action}")
-                }
-            }
-            true
+        recyclerViewTransferManager = CXRecyclerViewTransferManager(recyclerViewDayDetail, recyclerViewDays, { fromRecyclerView: RecyclerView, fromPosition: Int ->
+            dayDetailAdapter.remove(fromPosition)
+        }) { toRecyclerView: RecyclerView, fromPosition: Int, toPosition: Int ->
+            daysAdapter.add(dayDetailList[fromPosition], toPosition)
+            toRecyclerView.scrollToPosition(toPosition)
         }
     }
 }

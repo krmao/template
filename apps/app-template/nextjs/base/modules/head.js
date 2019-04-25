0803fd8e4d124d631f4da07f4dd1c4df62be867d
kr.mao <kr.mao@ctrip.com>
2019-04-25 13:26:43 +0800

    [nextjs] add banner

--- apps/app-template/nextjs/base/modules/head.js
+++ apps/app-template/nextjs/base/modules/head.js
@@ -1,15 +1,45 @@
 import React from "react";
 
+import BannerAnim from "rc-banner-anim";
+import QueueAnim from "rc-queue-anim";
+import TweenOne from "rc-tween-one";
+
+import "rc-banner-anim/dist/rc-banner-anim.css";
+import "./head-banner.css";
+
 import css from "./head.scss";
+
+const {Element, Thumb} = BannerAnim;
+const BgElement = Element.BgElement;
 
 export default class extends React.Component {
     constructor(props) {
         super(props);
-
-        this.state = {};
+        this.imgArray = [
+            "https://os.alipayobjects.com/rmsportal/IhCNTqPpLeTNnwr.jpg",
+            "https://os.alipayobjects.com/rmsportal/uaQVvDrCwryVlbb.jpg"
+        ];
+        this.state = {
+            enter: false
+        };
     }
 
+    onMouseEnter = () => {
+        this.setState({
+            enter: true
+        });
+    };
+
+    onMouseLeave = () => {
+        this.setState({
+            enter: false
+        });
+    };
+
     render() {
+        const thumbChildren = this.imgArray.map((img, i) =>
+            <span key={i}><i style={{backgroundImage: `url(${img})`}}/></span>
+        );
         return (
             <div className={css.root}>
                 <div className={css.header}>
@@ -58,6 +88,57 @@ export default class extends React.Component {
                         </tbody>
 
                     </table>
+                </div>
+                <div className={css.banner}>
+                    <BannerAnim onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave}>
+                        <Element key="aaa" prefixCls="banner-user-elem">
+                            <BgElement
+                                key="bg"
+                                className="bg"
+                                style={{
+                                    backgroundImage: `url(${this.imgArray[0]})`,
+                                    backgroundSize: "cover",
+                                    backgroundPosition: "center"
+                                }}
+                            />
+                            <QueueAnim key="1" name="QueueAnim">
+                                <h1 key="h1">Ant Motion Demo</h1>
+                                <p key="p">Ant Motion Demo.Ant Motion Demo.Ant Motion Demo.Ant Motion Demo</p>
+                            </QueueAnim>
+                            <TweenOne
+                                animation={{y: 50, opacity: 0, type: "from", delay: 200}}
+                                key="2"
+                                name="TweenOne"
+                            >
+                                Ant Motion Demo.Ant Motion Demo
+                            </TweenOne>
+                        </Element>
+                        <Element key="bbb" prefixCls="banner-user-elem">
+                            <BgElement
+                                key="bg"
+                                className="bg"
+                                style={{
+                                    backgroundImage: `url(${this.imgArray[1]})`,
+                                    backgroundSize: "cover",
+                                    backgroundPosition: "center"
+                                }}
+                            />
+                            <QueueAnim key="1" name="QueueAnim">
+                                <h1 key="h1">Ant Motion Demo</h1>
+                                <p key="p">Ant Motion Demo.Ant Motion Demo.Ant Motion Demo.Ant Motion Demo</p>
+                            </QueueAnim>
+                            <TweenOne
+                                animation={{y: 50, opacity: 0, type: "from", delay: 200}}
+                                key="2"
+                                name="TweenOne"
+                            >
+                                Ant Motion Demo.Ant Motion Demo
+                            </TweenOne>
+                        </Element>
+                        <Thumb prefixCls="user-thumb" key="thumb" component={TweenOne} animation={{bottom: this.state.enter ? 0 : -70}}>
+                            {thumbChildren}
+                        </Thumb>
+                    </BannerAnim>
                 </div>
             </div>
         );
--- apps/app-template/nextjs/next.config.js
+++ apps/app-template/nextjs/next.config.js
@@ -1,4 +1,18 @@
-const withSass = require('@zeit/next-sass');
-module.exports = withSass({
-    cssModules: true
-});
+const withPlugins = require("next-compose-plugins");
+const typescript = require("@zeit/next-typescript");
+const css = require("@zeit/next-css");
+const sass = require("@zeit/next-sass");
+const images = require("next-images");
+
+const next = {
+    distDir: "./.next"
+};
+
+const config = withPlugins([
+    [typescript],
+    [css, {cssModules: false}],
+    [sass, {cssModules: true}],
+    [images]
+], next);
+
+module.exports = config;

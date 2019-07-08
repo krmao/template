1bbabc502a9ff5058da63197391720ae57d249e9
kr.mao <kr.mao@ctrip.com>
2019-07-08 17:46:00 +0800

    update eslint config

--- web/react_app/.eslintrc.js
+++ web/react_app/.eslintrc.js
@@ -30,13 +30,14 @@ module.exports = {
         "no-dupe-keys": "error", // 在创建对象字面量时不允许键重复
         "no-dupe-args": "error", // 函数参数不能重复
         "no-redeclare": "error", // 禁止重复声明变量
-        "no-unused-vars": ["off", {"vars": "all", "args": "after-used"}],
         "no-const-assign": "error", // 禁止修改const声明的变量
+        "no-useless-concat": "off", // 没有必要将两个字符串连接在一起
         "no-duplicate-case": "error", // switch中的case标签不能重复
         "no-trailing-spaces": "warn", // 一行结束后面有空格就发出警告
         "no-underscore-dangle": "error", // 标识符不能以_开头或结尾
         "no-use-before-define": "off", // 未定义前不能使用
         "no-mixed-spaces-and-tabs": "error", // 禁止混用tab和空格
+        "no-unused-vars": ["off", {"vars": "all", "args": "after-used"}], // 检测到未被使用的变量
         "jsx-quotes": ["error", "prefer-double"], // 强制在JSX属性（jsx-quotes）中一致使用双引号
     },
     "settings": {

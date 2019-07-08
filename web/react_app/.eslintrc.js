9d09dfbf0a0d822e8e119af2cc3aa7a961f4be1b
kr.mao <kr.mao@ctrip.com>
2019-07-08 17:33:45 +0800

    update eslint config

--- web/react_app/.eslintrc.js
+++ web/react_app/.eslintrc.js
@@ -15,46 +15,31 @@ module.exports = {
         "ecmaVersion": 2018,
         "sourceType": "module"
     },
-    "plugins": [
-        "react"
-    ],
-    "rules": {// 0=off, 1==warn, 2==error
-        "indent": [
-            "error",
-            4
-        ],
-        "eqeqeq": ["error", "smart"], // 全等于
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "no-trailing-spaces": 1, //一行结束后面有空格就发出警告
-        "no-undef": 0,
-        "no-var": 2, //对var警告
+    "plugins": ["react"],
+    "rules": { // off:0, warn:1, error:2
+        "camelcase": "warn", // 强制驼峰法命名
+        "use-isnan": "error", // 只能使用 isNaN() 进行比较
+        "indent": ["error", 4], // 缩进强制使用 4 个空格
+        "semi": ["error", "always"], // 强制使用双引号
+        "eqeqeq": ["error", "smart"], // 全等于, 使用智能模式 0==1 不报错
+        "quotes": ["error", "double"],
+        "linebreak-style": ["error", "unix"],
+        "no-var": "error", // 对var警告
+        "no-undef": "off", // 对检测到使用 未定义的变量 报错
+        "no-console": "off", // 对检测到使用 console 打印日志 报错
+        "no-dupe-keys": "error", // 在创建对象字面量时不允许键重复
+        "no-dupe-args": "error", // 函数参数不能重复
+        "no-redeclare": "error", // 禁止重复声明变量
         "no-unused-vars": ["off", {"vars": "all", "args": "after-used"}],
-        "no-underscore-dangle": 2, //标识符不能以_开头或结尾
-        "no-const-assign": 2, //禁止修改const声明的变量
-        "no-use-before-define": 0, //未定义前不能使用
-        "camelcase": 1, //强制驼峰法命名
-        "no-dupe-keys": 2, //在创建对象字面量时不允许键重复
-        "no-duplicate-case": 2, //switch中的case标签不能重复
-        "no-dupe-args": 2, //函数参数不能重复
-        "no-redeclare": 2, //禁止重复声明变量
-        "use-isnan": 2,//禁止比较时使用NaN，只能用isNaN()
-        "no-mixed-spaces-and-tabs": 2, //禁止混用tab和空格
-        "jsx-quotes": [2, "prefer-double"], //强制在JSX属性（jsx-quotes）中一致使用双引号
+        "no-const-assign": "error", // 禁止修改const声明的变量
+        "no-duplicate-case": "error", // switch中的case标签不能重复
+        "no-trailing-spaces": "warn", // 一行结束后面有空格就发出警告
+        "no-underscore-dangle": "error", // 标识符不能以_开头或结尾
+        "no-use-before-define": "off", // 未定义前不能使用
+        "no-mixed-spaces-and-tabs": "error", // 禁止混用tab和空格
+        "jsx-quotes": ["error", "prefer-double"], // 强制在JSX属性（jsx-quotes）中一致使用双引号
     },
     "settings": {
-        "import/ignore": [
-            "node_modules"
-        ]
+        "import/ignore": ["node_modules"]
     }
 };

c4711137b9f4a4556975138a6e057bc1a2299bcd
kr.mao <kr.mao@ctrip.com>
2019-09-30 18:19:16 +0800

    optimized code

--- mobile/IOS/App/App/App/AppDelegate.m
+++ mobile/IOS/App/App/App/AppDelegate.m
@@ -34,19 +34,21 @@
 //    STNavigationController *rootViewNavigationController = [[STNavigationController alloc] initWithRootViewController:homeController];
 //    rootViewNavigationController.navigationBarHidden = YES;
 ////    [FlutterRouter sharedInstance].navigationController = rootViewNavigationController;
-//
 //    self.window.rootViewController = rootViewNavigationController;
 //    return YES;
 //}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    UINavigationController * rootViewController = [[UINavigationController  alloc]initWithRootViewController: [[MainViewController alloc] init]];
+    NSLog(@"didFinishLaunchingWithOptions");
+    
+    STNavigationController *rootViewNavigationController = [[STNavigationController alloc] initWithRootViewController: [[MainViewController alloc] init]];
+    rootViewNavigationController.navigationBarHidden = YES;
     
     self.window = [[UIWindow alloc] initWithFrame: [UIScreen mainScreen].bounds];
     [self.window setBackgroundColor:[UIColor whiteColor]];
     [self.window makeKeyAndVisible];
-    [self.window setRootViewController:rootViewController];
+    [self.window setRootViewController:rootViewNavigationController];
     
     [[[UIApplication sharedApplication] delegate] setWindow:self.window];
     return YES;
@@ -55,28 +57,29 @@
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    NSLog(@"applicationWillResignActive");
 }
-
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    NSLog(@"applicationDidEnterBackground");
 }
-
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    NSLog(@"applicationWillEnterForeground");
 }
 
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    NSLog(@"applicationDidBecomeActive");
 }
-
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    NSLog(@"applicationWillTerminate");
 }
-
 
 @end

b53b742c76a375666d013c09156accf19bb47de8
kr.mao <kr.mao@ctrip.com>
2019-10-14 15:37:04 +0800

    optimized style

--- mobile/android/app/libraries/library-map/src/main/java/com/smart/library/map/layer/STMapControlView.kt
+++ mobile/android/app/libraries/library-map/src/main/java/com/smart/library/map/layer/STMapControlView.kt
@@ -2,10 +2,13 @@ package com.smart.library.map.layer
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.Color
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.ImageView
 import android.widget.RelativeLayout
+import android.widget.TextView
 import com.smart.library.map.R
 import com.smart.library.map.model.STMapType
 import com.smart.library.util.STViewUtil
@@ -22,6 +25,8 @@ internal class STMapControlView @JvmOverloads constructor(context: Context, attr
         hideLoading()
     }
 
+    fun trafficText(): TextView = trafficText
+    fun trafficImage(): ImageView = trafficImage
     fun trafficBtn(): View = trafficBtn
     fun settingsBtn(): View = settingsBtn
     fun locationBtn(): View = locationBtn
@@ -39,6 +44,7 @@ internal class STMapControlView @JvmOverloads constructor(context: Context, attr
     fun setButtonClickedListener(_mapView: STMapView) {
         mapView = _mapView
         setSwitchMapBtnStyle()
+        setTrafficBtnStyle()
 
         switchMapButton().setOnClickListener {
             mapView?.switchTo(if (mapView?.mapType() == STMapType.BAIDU) STMapType.GAODE else STMapType.BAIDU) { _: Boolean, _: STMapType ->
@@ -49,16 +55,25 @@ internal class STMapControlView @JvmOverloads constructor(context: Context, attr
             mapView?.switchTheme()
         }
         trafficBtn().setOnClickListener {
-            mapView?.enableTraffic(!(mapView?.isTrafficEnabled() ?: false))
+            setTrafficBtnStyle(!isTrafficEnabled())
         }
 
         locationBtn.setOnClickListener(mapView?.onLocationButtonClickedListener())
         locationBtn.setOnLongClickListener(mapView?.onLocationButtonLongClickedListener())
     }
 
+    fun isTrafficEnabled(): Boolean = mapView?.isTrafficEnabled() ?: false
+
+    private fun setTrafficBtnStyle(isTrafficEnabled: Boolean = isTrafficEnabled()) {
+        mapView?.enableTraffic(isTrafficEnabled)
+        trafficImage().setImageResource(if (isTrafficEnabled) R.drawable.st_traffic_on else R.drawable.st_traffic)
+        trafficText().setTextColor(Color.parseColor(if (isTrafficEnabled) "#1296db" else "#000000"))
+    }
+
     private fun setSwitchMapBtnStyle() {
-        switchMapBtnImage.setImageResource(if (mapView?.mapType() == STMapType.BAIDU) R.drawable.st_gaode else R.drawable.st_baidu)
+        switchMapBtnImage.setImageResource(if (mapView?.mapType() == STMapType.BAIDU) R.drawable.st_gaode_on else R.drawable.st_baidu_on)
         switchMapBtnTV.text = if (mapView?.mapType() == STMapType.BAIDU) "切换高德" else "切换百度"
+        switchMapBtnTV.setTextColor(Color.parseColor(if (mapView?.mapType() == STMapType.BAIDU) "#1296db" else "#d81e06"))
     }
 
     fun hideLoading() {

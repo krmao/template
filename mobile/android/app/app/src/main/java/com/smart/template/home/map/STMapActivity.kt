76b175b5b50c11e17b3c24a63b76531d19e05823
kr.mao <kr.mao@ctrip.com>
2019-11-06 11:06:29 +0800

    RESOLVE ERRORS

--- mobile/android/app/app/src/main/java/com/smart/template/home/map/STMapActivity.kt
+++ mobile/android/app/app/src/main/java/com/smart/template/home/map/STMapActivity.kt
@@ -1,21 +1,15 @@
 package com.smart.template.home.map
 
-import com.smart.template.home.map.model.STMapMode
-import com.smart.template.home.map.presenter.STMapCityPresenter
-import com.smart.template.home.map.presenter.STMapMultiHorizontalPresenter
 import com.smart.template.home.map.layer.STMapLayerManager
+import com.smart.template.home.map.layer.STMapLayerType
+import com.smart.template.home.map.layer.impl.STMapLayerCity
 
 class STMapActivity {
-
-    private val targetMode: STMapMode = STMapMode.CITY
+    private val layerType: STMapLayerType = STMapLayerType.CITY
 
     init {
-
-        if (targetMode == STMapMode.CITY) {
-            STMapLayerManager.push(STMapCityPresenter())
-            STMapLayerManager.push(STMapMultiHorizontalPresenter())
+        if (layerType == STMapLayerType.CITY) {
+            STMapLayerManager.push(STMapLayerCity())
         }
-
-
     }
 }

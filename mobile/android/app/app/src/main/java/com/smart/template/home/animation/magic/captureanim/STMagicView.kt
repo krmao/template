c100ec532b6dfa4dc07d8eb2f00cf9f652096e86
kr.mao <kr.mao@ctrip.com>
2020-03-13 15:41:32 +0800

    optimized magic

--- mobile/android/app/app/src/main/java/com/smart/template/home/animation/magic/captureanim/STMagicView.kt
+++ mobile/android/app/app/src/main/java/com/smart/template/home/animation/magic/captureanim/STMagicView.kt
@@ -14,81 +14,28 @@ import com.smart.library.util.animation.STInterpolatorFactory
 import kotlin.math.min
 
 /**
- * 高仿 MAC OS 窗口最小化时使用的神奇效果
- * 参考 link{https://github.com/xinlyun/CaptureView}
+ * 神奇效果
+ * 窗口最小化时使用
+ * @see {@link https://github.com/xinlyun/CaptureView}
  */
 @Suppress("MemberVisibilityCanBePrivate", "unused")
 class STMagicView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : View(context, attrs, defStyleAttr) {
 
-    // 当 xml 中配置宽高为 wrap_content 时, 使用以下宽高
-    private val wrapWidth: Int by lazy { 300.toPxFromDp() }
-    private val wrapHeight: Int by lazy { 100.toPxFromDp() }
-    private var bitmap: Bitmap? = null
-    private val paint = Paint().apply { isAntiAlias = true }
-    private var meshHelper: STMeshHelper = STMeshHelper()
     private var progress = 0f
+    private var bitmap: Bitmap? = null
+    private var meshHelper: STMeshHelper = STMeshHelper()
 
-    override fun onDraw(canvas: Canvas) {
-        super.onDraw(canvas)
-        val innerBitmap = bitmap ?: return
-        val verts = meshHelper.setProgress(progress)
-        /**
-         * bitmap：将要扭曲的图像
-         * meshWidth：控制在横向上把该图像划成多少格
-         * meshHeight：控制在纵向上把该图像划成多少格
-         * verts：网格交叉点坐标数组，长度为(meshWidth + 1) * (meshHeight + 1) * 2
-         * vertOffset：控制verts数组中从第几个数组元素开始才对bitmap进行扭曲
-
-         *  https://www.jianshu.com/p/51d8dd99d27d
-         * https://www.jianshu.com/p/11e6be1f18e6
-         */
-        canvas.drawBitmapMesh(
-            innerBitmap,
-            meshHelper.meshWidth,
-            meshHelper.meshHeight,
-            verts,
-            0,
-            null,
-            0,
-            paint
-        )
-    }
-
-    /**
-     * 应用需求,需要根据在xml描述的宽高自适应故这么写,
-     *
-     * 不管父View是何模式，若子View有确切数值，则子View大小就是其本身大小，且mode是EXACTLY
-     * 若子View是match_parent，则模式与父View相同，且大小同父View（若父View是UNSPECIFIED，则子View大小为0）
-     * 若子View是wrap_content，则模式是AT_MOST，大小同父View，表示不可超过父View大小（若父View是UNSPECIFIED，则子View大小为0）
-     * 链接：https://www.jianshu.com/p/cecd0de7ec27
-     *
-     */
-    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        if (!isInEditMode) {
-            val measureWidth: Int = measureHandler(wrapWidth, widthMeasureSpec)
-            val measureHeight: Int = measureHandler(wrapHeight, heightMeasureSpec)
-            STLogUtil.d(TAG, "onMeasure measureWidth:$measureWidth  measureHeight:$measureHeight")
-            meshHelper.init(measureWidth.toFloat(), measureHeight.toFloat())
-            bitmap?.let { meshHelper.setBitmapSize(it.width, it.height) }
-        }
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-    }
-
-    private fun measureHandler(defaultSize: Int, measureSpec: Int): Int {
-        val specSize = MeasureSpec.getSize(measureSpec)
-        return when (MeasureSpec.getMode(measureSpec)) {
-            MeasureSpec.EXACTLY -> specSize
-            MeasureSpec.AT_MOST -> min(defaultSize, specSize)
-            else -> defaultSize
-        }
-    }
+    private val defaultDuration: Long = 800
+    private val defaultWidth: Int by lazy { 300.toPxFromDp() }  // 当 xml 中配置宽高为 wrap_content 时, 使用以下宽高
+    private val defaultHeight: Int by lazy { 100.toPxFromDp() }
+    private val defaultPaint = Paint().apply { isAntiAlias = true }
+    private val defaultInterpolator: Interpolator = STInterpolatorFactory.createAccelerateDecelerateInterpolator()
 
     private var onProgressListener: ((progress: Float) -> Unit)? = null
-
-    val animator: ValueAnimator by lazy {
+    private val animator: ValueAnimator by lazy {
         ValueAnimator.ofFloat(0f, 1f).apply {
-            duration = 800
-            interpolator = STInterpolatorFactory.createAccelerateDecelerateInterpolator()
+            duration = defaultDuration
+            interpolator = defaultInterpolator
             addUpdateListener { animation ->
                 val progress = animation.animatedValue as Float
                 setProgress(progress)
@@ -99,12 +46,66 @@ class STMagicView @JvmOverloads constructor(context: Context, attrs: AttributeSe
         }
     }
 
-    fun setDuration(duration: Long = 800): STMagicView {
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        val innerBitmap = bitmap ?: return
+
+        /**
+         * 扭曲图像, 使用网格绘制法
+         *
+         * bitmap：      将要扭曲的图像
+         * meshWidth：   控制在横向上把该图像划成多少格
+         * meshHeight：  控制在纵向上把该图像划成多少格
+         * verts：       网格交叉点坐标数组，长度为(meshWidth + 1) * (meshHeight + 1) * 2
+         * vertOffset：  控制verts数组中从第几个数组元素开始才对bitmap进行扭曲
+         *
+         * @see @{link https://www.jianshu.com/p/51d8dd99d27d}
+         * @see @{link https://www.jianshu.com/p/11e6be1f18e6}
+         */
+        canvas.drawBitmapMesh(
+            innerBitmap,
+            meshHelper.meshWidth,
+            meshHelper.meshHeight,
+            meshHelper.setProgress(progress),
+            0,
+            null,
+            0,
+            defaultPaint
+        )
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        if (!isInEditMode) {
+            val measureWidth: Int = measureHandler(defaultWidth, widthMeasureSpec)
+            val measureHeight: Int = measureHandler(defaultHeight, heightMeasureSpec)
+            STLogUtil.d(TAG, "onMeasure measureWidth:$measureWidth  measureHeight:$measureHeight")
+            meshHelper.init(measureWidth.toFloat(), measureHeight.toFloat())
+            bitmap?.let { meshHelper.setBitmapSize(it.width.toFloat(), it.height.toFloat()) }
+        }
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+    }
+
+    /**
+     * 不管父View是何模式，若子View有确切数值，则子View大小就是其本身大小，且mode是EXACTLY
+     * 若子View是match_parent，则模式与父View相同，且大小同父View（若父View是UNSPECIFIED，则子View大小为0）
+     * 若子View是wrap_content，则模式是AT_MOST，大小同父View，表示不可超过父View大小（若父View是UNSPECIFIED，则子View大小为0）
+     * @see {@link https://www.jianshu.com/p/cecd0de7ec27}
+     */
+    private fun measureHandler(defaultSize: Int, measureSpec: Int): Int {
+        val specSize = MeasureSpec.getSize(measureSpec)
+        return when (MeasureSpec.getMode(measureSpec)) {
+            MeasureSpec.EXACTLY -> specSize
+            MeasureSpec.AT_MOST -> min(defaultSize, specSize)
+            else -> defaultSize
+        }
+    }
+
+    fun setDuration(duration: Long = defaultDuration): STMagicView {
         animator.duration = duration
         return this
     }
 
-    fun setInterpolator(interpolator: Interpolator = STInterpolatorFactory.createAccelerateDecelerateInterpolator()): STMagicView {
+    fun setInterpolator(interpolator: Interpolator = defaultInterpolator): STMagicView {
         animator.interpolator = interpolator
         return this
     }
@@ -113,6 +114,9 @@ class STMagicView @JvmOverloads constructor(context: Context, attrs: AttributeSe
         this.onProgressListener = onProgressListener
     }
 
+    /**
+     * @param progress [0, 1]
+     */
     fun setProgress(progress: Float): STMagicView {
         this.progress = progress
         invalidate()
@@ -121,7 +125,7 @@ class STMagicView @JvmOverloads constructor(context: Context, attrs: AttributeSe
 
     fun setBitmap(bitmap: Bitmap): STMagicView {
         this.bitmap = bitmap
-        meshHelper.setBitmapSize(bitmap.width, bitmap.height)
+        meshHelper.setBitmapSize(bitmap.width.toFloat(), bitmap.height.toFloat())
         return this
     }
 
@@ -129,7 +133,7 @@ class STMagicView @JvmOverloads constructor(context: Context, attrs: AttributeSe
     fun isStarted(): Boolean = animator.isStarted
 
     fun start() {
-        if (isRunning()) {
+        if (isRunning() || isStarted()) {
             animator.cancel()
         }
         animator.start()
--- mobile/android/app/app/src/main/java/com/smart/template/home/animation/magic/captureanim/STMeshHelper.kt
+++ mobile/android/app/app/src/main/java/com/smart/template/home/animation/magic/captureanim/STMeshHelper.kt
@@ -1,14 +1,13 @@
 package com.smart.template.home.animation.magic.captureanim
 
 import android.view.animation.Interpolator
-import androidx.interpolator.view.animation.FastOutSlowInInterpolator
 import com.smart.library.util.STLogUtil
 import com.smart.library.util.animation.STInterpolatorFactory
 
 /**
- * 供给CaptureAnimView的支持类,核心数据计算均在此处
- * https://github.com/xinlyun/CaptureView
- * https://www.jianshu.com/p/51d8dd99d27d
+ * 扭曲图像, 网格绘制帮助类
+ * @see {@link https://github.com/xinlyun/CaptureView}
+ * @see {@link https://www.jianshu.com/p/51d8dd99d27d}
  */
 class STMeshHelper {
 
@@ -18,26 +17,48 @@ class STMeshHelper {
     private var bitmapFitHeight: Float = 0f
 
     /**
-     * 网格坐标
-     * https://www.jianshu.com/p/11e6be1f18e6
-     * https://www.jianshu.com/p/51d8dd99d27d
+     * 将图像分隔为多少格
+     * @see {@link https://www.jianshu.com/p/11e6be1f18e6}
+     * @see {@link https://www.jianshu.com/p/51d8dd99d27d}
      */
-    var meshWidth: Int = 40
+    var meshWidth: Int = 50
         private set
-    var meshHeight: Int = 40
+    var meshHeight: Int = 50
         private set
 
-    fun init(viewMeasureWidth: Float, viewMeasureHeight: Float, meshWidth: Int = 80, meshHeight: Int = 80) {
+    fun init(viewMeasureWidth: Float, viewMeasureHeight: Float, meshWidth: Int = this.meshWidth, meshHeight: Int = this.meshHeight) {
         this.viewMeasureWidth = viewMeasureWidth
         this.viewMeasureHeight = viewMeasureHeight
         this.meshWidth = meshWidth
         this.meshHeight = meshHeight
     }
 
-    fun setBitmapSize(bitmapWidth: Int, bitmapHeight: Int) {
-        this.bitmapFitWidth = viewMeasureWidth
-        this.bitmapFitHeight = viewMeasureWidth / bitmapWidth * bitmapHeight
-        STLogUtil.d(STMagicView.TAG, "setBitmapSize bitmapWidth:$bitmapWidth  bitmapHeight:$bitmapHeight, viewMeasureWidth=$viewMeasureWidth, viewMeasureHeight=$viewMeasureHeight, bitmapFitWidth=$bitmapFitWidth, bitmapFitHeight=$bitmapFitHeight")
+    /**
+     * 输入图像宽高, 使得图像自适应 view 的宽高, 相当于 fitTop
+     */
+    fun setBitmapSize(bitmapWidth: Float, bitmapHeight: Float) {
+        val innerViewMeasureWidth: Float = viewMeasureWidth
+        val innerViewMeasureHeight: Float = viewMeasureHeight
+        if (innerViewMeasureWidth == 0f || innerViewMeasureHeight == 0f || bitmapWidth == 0f || bitmapHeight == 0f) return
+
+        val viewAspectRatio: Float = innerViewMeasureWidth / innerViewMeasureHeight
+        val bitmapAspectRatio: Float = bitmapWidth / bitmapHeight
+
+        when {
+            bitmapAspectRatio > viewAspectRatio -> {
+                this.bitmapFitWidth = innerViewMeasureWidth
+                this.bitmapFitHeight = innerViewMeasureWidth / bitmapAspectRatio
+            }
+            bitmapAspectRatio < viewAspectRatio -> {
+                this.bitmapFitWidth = innerViewMeasureHeight * bitmapAspectRatio
+                this.bitmapFitHeight = innerViewMeasureHeight
+            }
+            else -> {
+                this.bitmapFitWidth = innerViewMeasureWidth
+                this.bitmapFitHeight = innerViewMeasureHeight
+            }
+        }
+        STLogUtil.d(STMagicView.TAG, "setBitmapSize bitmapWidth:$bitmapWidth  bitmapHeight:$bitmapHeight, viewMeasureWidth=$viewMeasureWidth, viewMeasureHeight=$viewMeasureHeight, bitmapFitWidth=$bitmapFitWidth, bitmapFitHeight=$bitmapFitHeight, viewAspectRatio=$viewAspectRatio, bitmapAspectRatio=$bitmapAspectRatio")
     }
 
     /**
@@ -55,13 +76,13 @@ class STMeshHelper {
      * |   \              /          |
      * |    |            /rightLine  |
      * |    |           /            |
-     * |    |          /
+     * | ->->|          / <-<-<-<
      * | 0.1f        0.3f
-     * |
+     * | leftLine    rightLine
      * @param progress
-     * @return
+     * @return verts 图片被分割成网格的坐标数组 x,y,x,y ...
      */
-    fun setProgress(progress: Float, leftToX: Float = 0.85f, rightToX: Float = 0.9f): FloatArray {
+    fun setProgress(progress: Float, leftLintToXRatio: Float = 0.85f, rightLineToXRatio: Float = 0.9f): FloatArray {
         //左右边线运动轨迹
         val leftLine: LineProgress
         val rightLine: LineProgress
@@ -71,57 +92,71 @@ class STMeshHelper {
             // leftLine = LinePosi(0f, width * 0.1f * (progress / 0.3f), 0f, height)
             // rightLine = LinePosi(width, width * 0.2f + width * 0.8f * (0.3f - progress) / 0.3f, 0f, height)
 
-            leftLine = LineProgress(0f, viewMeasureWidth * leftToX * (progress / 0.3f), 0f, viewMeasureHeight)
-            rightLine = LineProgress(viewMeasureWidth, viewMeasureWidth * (1 - (1 - rightToX) * (progress / 0.3f)), 0f, viewMeasureHeight)
+            leftLine = LineProgress(0f, viewMeasureWidth * leftLintToXRatio * (progress / 0.3f), 0f, viewMeasureHeight)
+            rightLine = LineProgress(viewMeasureWidth, viewMeasureWidth * (1 - (1 - rightLineToXRatio) * (progress / 0.3f)), 0f, viewMeasureHeight)
         } else {
             //在0.3f~1f,左右轨迹保持不变,图像按照此轨迹作为边界进行运动
             // leftLine = LinePosi(0f, width * 0.1f, 0f, height)
             // rightLine = LinePosi(width, width * 0.2f, 0f, height)
-            leftLine = LineProgress(0f, viewMeasureWidth * leftToX, 0f, viewMeasureHeight)
-            rightLine = LineProgress(viewMeasureWidth, viewMeasureWidth * rightToX, 0f, viewMeasureHeight)
+            leftLine = LineProgress(0f, viewMeasureWidth * leftLintToXRatio, 0f, viewMeasureHeight)
+            rightLine = LineProgress(viewMeasureWidth, viewMeasureWidth * rightLineToXRatio, 0f, viewMeasureHeight)
         }
-        val progressDestY = viewMeasureHeight * progress
+
+        // 首先计算出 这条线 在 view 中 Y 轴应该出现的位置
+        val progressYInView: Float = viewMeasureHeight * progress
+
+        // 初始化图片被分割为网格后的 verts 数组
         val verts = FloatArray((meshWidth + 1) * (meshHeight + 1) * 2)
         var num = 0
-        for (i in 0..meshHeight) {
-            for (j in 0..meshWidth) {
+        for (meshHeightIndex in 0..meshHeight) {
+            for (meshWidthIndex in 0..meshWidth) {
+                // 计算出扭曲后的 bitmap 网格 Y 坐标
+                val progressYInBitmap: Float = progressYInView + bitmapFitHeight * meshHeightIndex / meshHeight
 
-                //Y轴点位根据实际bitmap高度进行平均
-                val progressY = progressDestY + bitmapFitHeight * i / meshHeight
-                val progressLeftX = leftLine.inputY(progressY)
-                val progressRightX = rightLine.inputY(progressY)
-                val progressDistanceX = progressRightX - progressLeftX
+                val progressLeftLineXInBitmap: Float = leftLine.calculateProgressXByProgressY(progressYInBitmap)
+                val progressRightLineXInBitmap: Float = rightLine.calculateProgressXByProgressY(progressYInBitmap)
+                val progressDistanceXInBitmap: Float = progressRightLineXInBitmap - progressLeftLineXInBitmap
 
-                //X点位根据两个line在Y值时的差值进行平均分布
-                val progressX = progressDistanceX * j / meshWidth + progressLeftX
+                // 当前 bitmap 网格宽度比例
+                val meshWidthRatio: Float = meshWidthIndex / meshWidth.toFloat() // 一定要 float, 否则整除计算出错
+                // 计算出扭曲后的 bitmap 网格 X 坐标
+                val progressXInBitmap: Float = progressLeftLineXInBitmap + progressDistanceXInBitmap * meshWidthRatio
 
-                //先X后Y输出
-                verts[num] = progressX
-                num++
-                verts[num] = progressY
-                num++
+                verts[num++] = progressXInBitmap
+                verts[num++] = progressYInBitmap
             }
         }
         return verts
     }
 
-
     private val fastOutSlowInInterpolator: Interpolator by lazy { STInterpolatorFactory.createFastOutSlowInInterpolator() } // 从 0f -> 1f 的过程是 先加速再减速
-    // private val fastOutLinearInInterpolator: Interpolator by lazy { FastOutLinearInInterpolator() } // 从 0f -> 1f 的过程是 先加速然后匀速
-    // private val linearOutSlowInInterpolator: Interpolator by lazy { LinearOutSlowInInterpolator() } // 从 0f -> 1f 的过程是 先匀速再减速
-    // private val accelerateDecelerateInterpolator: Interpolator by lazy { AccelerateDecelerateInterpolator() }
 
     /**
-     * 描述运动轨迹(起点XY和终点XY以及变函数Interpolator),根据输入量,可输出相对应的值
+     * 描述一条线的运动轨迹, 比如左侧边界线, 右侧边界线
+     *
+     * @param fromX 曲线起点 X 轴坐标
+     * @param fromY 曲线起点 Y 轴坐标
+     * @param toX   曲线终点 X 轴坐标
+     * @param toY   曲线终点 Y 轴坐标
      */
-    private inner class LineProgress internal constructor(private val fromX: Float, private val toX: Float, private val fromY: Float, private val toY: Float) {
-        fun inputY(progressY: Float): Float {
-            val disLength = toY - fromY
-            val disXLength = toX - fromX
-            val disFloat = progressY / disLength
-            val disXFloat = fastOutSlowInInterpolator.getInterpolation(disFloat)
-            return fromX + disXLength * disXFloat
-        }
+    private inner class LineProgress(private val fromX: Float, private val toX: Float, private val fromY: Float, private val toY: Float) {
 
+        // 这条线 Y 轴起点到终点总跨越长度
+        private val distanceY = toY - fromY
+
+        // 这条线 X 轴起点到终点总跨越长度
+        private val distanceX = toX - fromX
+
+        /**
+         * 根据这条线 当前 Y 轴已经移动的距离 计算出与 X 轴应该出现的位置
+         */
+        fun calculateProgressXByProgressY(progressY: Float): Float {
+            // 根据这条线 当前 Y 轴已经移动的距离 计算出与 Y 轴总长度对应的比例
+            val ratioY = progressY / distanceY
+            // 通过插值器获取这条线 X 轴对应应该移动的比例
+            val ratioX = fastOutSlowInInterpolator.getInterpolation(ratioY)
+            // 计算这条线 X 轴应该出现的位置
+            return fromX + distanceX * ratioX
+        }
     }
 }

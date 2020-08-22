352790253d6e54476af2e6820ca0b624d11fb3a1
kr.mao <kr.mao@ctrip.com>
2020-08-22 13:53:06 +0800

    理解了 eureka-server 与 zuul 的关系

--- service/springcloud/components/springcloud-discovery/eureka-server/src/main/kotlin/com/smart/springcloud/discovery/eurekaserver/EurekaServerApplication.kt
+++ service/springcloud/components/springcloud-discovery/eureka-server/src/main/kotlin/com/smart/springcloud/discovery/eurekaserver/EurekaServerApplication.kt
@@ -4,6 +4,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
+/**
+ * 用于内部 eureka-client 之间互相访问
+ */
 @SpringBootApplication
 @EnableEurekaServer // 开启 eureka 服务
 class EurekaServerApplication

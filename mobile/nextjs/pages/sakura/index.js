2222a51d460c5c791002c289fa75ab5e3517d025
kr.mao <kr.mao@ctrip.com>
2020-10-29 23:09:01 +0800

    nextjs optimized for sakura

--- mobile/nextjs/pages/sakura/index.js
+++ mobile/nextjs/pages/sakura/index.js
@@ -18,10 +18,19 @@ class Sakura extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            sakuraAnimateFlag: true,
-            sakuraArr: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+            sakuraAnimateEnabled: true,
+            sakuraArrays: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
         };
-        this.sakuraImgArr = [];
+        this.sakuraImageArray = [
+            "/static/sakura/1.jpg",
+            "/static/sakura/2.jpg",
+            "/static/sakura/3.jpeg",
+            "/static/sakura/4.jpg",
+            "/static/sakura/5.jpeg",
+            "/static/sakura/6.jpg",
+            "/static/sakura/7.jpg",
+            "/static/sakura/8.jpg"
+        ];
         console.log("[LIFECYCLE](Sakura) constructor");
     }
 
@@ -38,12 +47,12 @@ class Sakura extends React.Component {
 
     componentDidMount() {
         console.log("[LIFECYCLE](Sakura) componentDidMount");
-        let that = this;
-       /* setTimeout(() => {
-            that.setState({
-                sakuraAnimateFlag: false
-            });
-        }, 7000);*/
+        // let that = this;
+        /* setTimeout(() => {
+             that.setState({
+                 sakuraAnimateEnabled: false
+             });
+         }, 7000);*/
 
     }
 
@@ -76,30 +85,30 @@ class Sakura extends React.Component {
 
     render() {
         console.log("[LIFECYCLE](Sakura) render");
-        let props = this.props;
+        // let props = this.props;
         return (
             <React.Fragment>
                 <Head>
                     <title>SAKURA</title>
                 </Head>
                 <div className={css.page}>
-                    <AnimateSakura sakuraDepData={{sakuraImgArr: this.sakuraImgArr}} animated={true}/>
-                    <AnimateSakura sakuraDepData={{sakuraImgArr: this.sakuraImgArr}} animated={true}/>
-                    <AnimateSakura sakuraDepData={{sakuraImgArr: this.sakuraImgArr}} animated={true}/>
-                    <AnimateSakura sakuraDepData={{sakuraImgArr: this.sakuraImgArr}} animated={true}/>
-                    <AnimateSakura sakuraDepData={{sakuraImgArr: this.sakuraImgArr}} animated={true}/>
-                    <AnimateSakura sakuraDepData={{sakuraImgArr: this.sakuraImgArr}} animated={true}/>
-                    <AnimateSakura sakuraDepData={{sakuraImgArr: this.sakuraImgArr}} animated={true}/>
-                    <AnimateSakura sakuraDepData={{sakuraImgArr: this.sakuraImgArr}} animated={true}/>
-                    <AnimateSakura sakuraDepData={{sakuraImgArr: this.sakuraImgArr}} animated={true}/>
-                    <AnimateSakura sakuraDepData={{sakuraImgArr: this.sakuraImgArr}} animated={true}/>
-                    <AnimateSakura sakuraDepData={{sakuraImgArr: this.sakuraImgArr}} animated={true}/>
-                    <AnimateSakura sakuraDepData={{sakuraImgArr: this.sakuraImgArr}} animated={true}/>
-                    <AnimateSakura sakuraDepData={{sakuraImgArr: this.sakuraImgArr}} animated={true}/>
-                    <AnimateSakura sakuraDepData={{sakuraImgArr: this.sakuraImgArr}} animated={true}/>
-                    <AnimateSakura sakuraDepData={{sakuraImgArr: this.sakuraImgArr}} animated={true}/>
-                    <AnimateSakura sakuraDepData={{sakuraImgArr: this.sakuraImgArr}} animated={true}/>
-                    <AnimateSakura sakuraDepData={{sakuraImgArr: this.sakuraImgArr}} animated={true}/>
+                    <AnimateSakura data={{sakuraImageArray: this.sakuraImageArray}} animated={true}/>
+                    <AnimateSakura data={{sakuraImageArray: this.sakuraImageArray}} animated={true}/>
+                    <AnimateSakura data={{sakuraImageArray: this.sakuraImageArray}} animated={true}/>
+                    <AnimateSakura data={{sakuraImageArray: this.sakuraImageArray}} animated={true}/>
+                    <AnimateSakura data={{sakuraImageArray: this.sakuraImageArray}} animated={true}/>
+                    <AnimateSakura data={{sakuraImageArray: this.sakuraImageArray}} animated={true}/>
+                    <AnimateSakura data={{sakuraImageArray: this.sakuraImageArray}} animated={true}/>
+                    <AnimateSakura data={{sakuraImageArray: this.sakuraImageArray}} animated={true}/>
+                    <AnimateSakura data={{sakuraImageArray: this.sakuraImageArray}} animated={true}/>
+                    <AnimateSakura data={{sakuraImageArray: this.sakuraImageArray}} animated={true}/>
+                    <AnimateSakura data={{sakuraImageArray: this.sakuraImageArray}} animated={true}/>
+                    <AnimateSakura data={{sakuraImageArray: this.sakuraImageArray}} animated={true}/>
+                    <AnimateSakura data={{sakuraImageArray: this.sakuraImageArray}} animated={true}/>
+                    <AnimateSakura data={{sakuraImageArray: this.sakuraImageArray}} animated={true}/>
+                    <AnimateSakura data={{sakuraImageArray: this.sakuraImageArray}} animated={true}/>
+                    <AnimateSakura data={{sakuraImageArray: this.sakuraImageArray}} animated={true}/>
+                    <AnimateSakura data={{sakuraImageArray: this.sakuraImageArray}} animated={true}/>
                 </div>
             </React.Fragment>
         );

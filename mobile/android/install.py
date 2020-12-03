f5031127ea5098b4b27c2ba7148d45b04943160d
kr.mao <kr.mao@ctrip.com>
2020-12-03 16:43:59 +0800

    optimized install.py

--- mobile/android/install.py
+++ mobile/android/install.py
@@ -1,50 +1,74 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-import os
+import getopt
 import os.path
-import shutil
-import sys, getopt
+import sys
 
 if __name__ == '__main__':
-    opts, args = getopt.getopt(sys.argv[1:], 'hf:c:', ['f=','fv=', 'c=', 'help'])
+    opts, args = getopt.getopt(sys.argv[1:], 'hf:fv:c:r:i:', ['f=', 'fv=', 'c=', 'r=', 'i=', 'help'])
 
     flutter = True
     clean = True
+    release = True
+    info = True
     flutter_version = "0.0.1-SNAPSHOT"
 
     for key, value in opts:
-
         if key in ['-h', '--help']:
             print ''
-            print '参数:'
-            print '-h \t 显示帮助'
-            print '-f \t .fvm/flutter_sdk/bin/flutter build aar --debug --build-number 0.0.1-SNAPSHOT --pub --verbose'
-            print '-c \t ./gradlew clean installDebug --info --stacktrace'
+            print 'arguments:'
+            print '-h \t show help'
+            print '-f \t rebuild flutter aar'
+            print '-fv \t flutter aar version'
+            print '-c \t clean'
+            print '-r \t install release'
+            print '-i \t show log'
             sys.exit(0)
         if key in ['-f', '--flutter']:
-            flutter = value == "Trues"
+            flutter = value == "true" or value == "True" or value == "1"
         if key in ['-fv', '--flutter-version']:
             flutter_version = value
         if key in ['-c', '--clean']:
-            clean = value == "True"
+            clean = value == "true" or value == "True" or value == "1"
+        if key in ['-c', '--clean']:
+            release = value == "true" or value == "True" or value == "1"
+        if key in ['-i', '--info']:
+            info = value == "true" or value == "True" or value == "1"
 
-    print 'clean:', clean, ',flutter:', flutter, 'flutter-version:', flutter_version
-
-    print("---- install start ----")
-
-    print os.getcwd()
+    print "==========>>>>>>>>>> install start"
+    print 'arguments -> clean:', clean, ',flutter:', flutter, 'flutter-version:', flutter_version, 'release:', release
 
     if flutter:
         os.chdir("../flutter_module")
-        print os.getcwd()
-        os.system("fvm install")
-        os.system(".fvm/flutter_sdk/bin/flutter build aar --debug --build-number " + flutter_version +" --pub --verbose")
+        if not os.path.exists(".fvm/flutter_sdk/bin/flutter"):
+            print "==========>>>>>>>>>> install flutter sdk start"
+            print "current path ->", os.getcwd()
+            shell_install_flutter_sdk = "fvm install"
+            print shell_install_flutter_sdk
+            os.system(shell_install_flutter_sdk)
+            print "==========>>>>>>>>>> install flutter sdk end"
+        print "==========>>>>>>>>>> build flutter lib aar start"
+        print "current path ->", os.getcwd()
+        shell_build_flutter_aar = ".fvm/flutter_sdk/bin/flutter build aar --" + ("debug --no-profile --no-release" if release else "release --no-profile --no-debug") + " --build-number " + flutter_version + " --pub " + ("" if info else "--verbose")
+        print shell_build_flutter_aar
+        os.system(shell_build_flutter_aar)
+        print "==========>>>>>>>>>> build flutter lib aar end"
         os.chdir("../android")
-        print os.getcwd()
+    print "==========>>>>>>>>>> install android app start"
+    print "current path ->", os.getcwd()
     if clean:
-        os.system("./gradlew clean installDebug --info --stacktrace")
-    else :
-        os.system("./gradlew installDebug --info --stacktrace")
-
-    print("---- install end ----")
+        shell_install = "./gradlew clean install" + ("Debug" if release else "Release") + (" " if info else " --info") + " --stacktrace"
+        print shell_install
+        os.system(shell_install)
+    else:
+        shell_install = "./gradlew install" + ("Debug" if release else "Release") + (" " if info else " --info") + " --stacktrace"
+        print shell_install
+        os.system(shell_install)
+    print "==========>>>>>>>>>> install android app end"
+    print "==========>>>>>>>>>> auto open android app start"
+    shell_install = """adb shell am start -n "com.smart.template/com.smart.template.FinalLaunchActivity" -a android.intent.action.MAIN -c android.intent.category.LAUNCHER"""
+    print shell_install
+    os.system(shell_install)
+    print "==========>>>>>>>>>> auto open android app end"
+    print("==========>>>>>>>>>> install end")

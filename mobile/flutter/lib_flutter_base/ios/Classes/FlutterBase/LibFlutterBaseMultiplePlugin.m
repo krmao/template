dd304210c640a444eb7f22883d05a98696c091d3
kr.mao <kr.mao@ctrip.com>
2021-03-20 11:56:57 +0800

    optmized code

--- mobile/flutter/lib_flutter_base/ios/Classes/FlutterBase/LibFlutterBaseMultiplePlugin.m
+++ mobile/flutter/lib_flutter_base/ios/Classes/FlutterBase/LibFlutterBaseMultiplePlugin.m
@@ -2,6 +2,7 @@
 #import "FlutterBoostPlugin.h"
 #import "STFlutterPlugin.h"
 
+static NSString * BRIDGE_EVENT_NAME = @"__codesdancing_flutter_event__";
 @interface LibFlutterBaseMultiplePlugin()
 @end
 
@@ -11,36 +12,36 @@
     NSArray *array = [call.method componentsSeparatedByString:@"-"];
     NSString *moduleName = [array firstObject];
     NSString *functionName = [call.method stringByReplacingOccurrencesOfString:[NSString stringWithFormat:@"%@-",moduleName] withString:@""];
-    NSLog(@"handleMethodCall moduleName=%@, functionName=%@, arguments=%@", moduleName, functionName, call.arguments);
+    NSLog(@"[page] handleMethodCall moduleName=%@, functionName=%@, arguments=%@", moduleName, functionName, call.arguments);
     [STFlutterPlugin callModule:moduleName function:functionName arguments:call.arguments result:result];
 }
 
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-    NSLog(@"LibFlutterBaseMultiplePlugin registerWithRegistrar start");
+    NSLog(@"[page] LibFlutterBaseMultiplePlugin registerWithRegistrar start");
     FlutterMethodChannel* channel = [FlutterMethodChannel
                                      methodChannelWithName:@"codesdancing.flutter.bridge/callNative"
                                      binaryMessenger:[registrar messenger]
                                      codec:[FlutterJSONMethodCodec sharedInstance]];
-    NSLog(@"LibFlutterBaseMultiplePlugin registerWithRegistrar start");
+    NSLog(@"[page] LibFlutterBaseMultiplePlugin registerWithRegistrar start");
     LibFlutterBaseMultiplePlugin* instance = [LibFlutterBaseMultiplePlugin sharedInstance];
     instance.methodChannel = channel;
     
     [registrar addMethodCallDelegate:instance channel:channel];
-    NSLog(@"LibFlutterBaseMultiplePlugin registerWithRegistrar end");
+    NSLog(@"[page] LibFlutterBaseMultiplePlugin registerWithRegistrar end");
 }
 
 + (LibFlutterBaseMultiplePlugin *)registerWithRegistrar2:(NSObject<FlutterPluginRegistrar>*)registrar {
-    NSLog(@"LibFlutterBaseMultiplePlugin registerWithRegistrar start");
+    NSLog(@"[page] LibFlutterBaseMultiplePlugin registerWithRegistrar start");
     FlutterMethodChannel* channel = [FlutterMethodChannel
                                      methodChannelWithName:@"codesdancing.flutter.bridge/callNative"
                                      binaryMessenger:[registrar messenger]
                                      codec:[FlutterJSONMethodCodec sharedInstance]];
-    NSLog(@"LibFlutterBaseMultiplePlugin registerWithRegistrar start");
+    NSLog(@"[page] LibFlutterBaseMultiplePlugin registerWithRegistrar2 start");
     LibFlutterBaseMultiplePlugin* newPlugin = [LibFlutterBaseMultiplePlugin new]; // 没有必要使用 sharedInstance, 因为下一步 methodChannel 已经被替换, 当前页面回收后, 上一个页面的 methodChannel 将为 nil
     newPlugin.methodChannel = channel;
     
     [registrar addMethodCallDelegate:newPlugin channel:channel];
-    NSLog(@"LibFlutterBaseMultiplePlugin registerWithRegistrar end");
+    NSLog(@"[page] LibFlutterBaseMultiplePlugin registerWithRegistrar2 end");
     return newPlugin;
 }
 
@@ -56,63 +57,59 @@
 + (LibFlutterBaseMultiplePlugin* )getPlugin:(FlutterEngine*)engine{
     BOOL hasPlugin = [engine hasPlugin:@"LibFlutterBaseMultiplePlugin"];
     NSObject *published = [engine valuePublishedByPlugin:@"LibFlutterBaseMultiplePlugin"];
-    NSLog(@"getPlugin hasPlugin=%d, engine=%@, published=%@",hasPlugin, engine, published);
+    NSLog(@"[page] getPlugin hasPlugin=%d, engine=%@, published=%@",hasPlugin, engine, published);
     
     if ([published isKindOfClass:[LibFlutterBaseMultiplePlugin class]]) {
         LibFlutterBaseMultiplePlugin *plugin = (LibFlutterBaseMultiplePlugin *)published;
-        NSLog(@"getPlugin return plugin=%@",plugin);
+        NSLog(@"[page] getPlugin return plugin=%@",plugin);
         return plugin;
     }
     
-    NSLog(@"getPlugin return plugin nil !");
+    NSLog(@"[page] getPlugin return plugin nil !");
     return nil;
 }
 
 + (void) sendEventToDart:(FlutterEngine*)engine eventKey:(NSString*) eventKey eventInfo:(NSDictionary*) eventInfo {
-    NSLog(@"sendEventToDart engine=%@, eventKey=%@, eventInfo=%@", engine, eventKey, eventInfo);
-
     LibFlutterBaseMultiplePlugin* plugin = [LibFlutterBaseMultiplePlugin getPlugin:engine];
     FlutterMethodChannel * methodChannel = plugin.methodChannel;
     
-    NSLog(@"sendEventToDart plugin=%@, methodChannel=%@",plugin, methodChannel);
+    NSLog(@"[page] sendEventToDart methodChannel=%@, eventKey=%@, eventInfo=%@", methodChannel, eventKey, eventInfo);
     
-    NSString * BRIDGE_EVENT_NAME = @"__codesdancing_flutter_event__";
     if (methodChannel == nil || [methodChannel isKindOfClass:[NSNull class]]) {
         methodChannel = [LibFlutterBaseMultiplePlugin sharedInstance].methodChannel;
     }
-    NSLog(@"sendEventToDart install plugin=%@, methodChannel=%@",[LibFlutterBaseMultiplePlugin sharedInstance], methodChannel);
+    NSLog(@"[page] sendEventToDart install plugin=%@, methodChannel=%@",[LibFlutterBaseMultiplePlugin sharedInstance], methodChannel);
     
     if (methodChannel != nil) {
         NSMutableDictionary* eventData = NSMutableDictionary.new;
         [eventData setValue:eventKey forKey:@"eventName"];
         [eventData setValue:eventInfo forKey:@"eventInfo"];
   
-        NSLog(@"sendEventToDart invokeMethod start BRIDGE_EVENT_NAME=%@",BRIDGE_EVENT_NAME);
+        NSLog(@"[page] sendEventToDart invokeMethod start BRIDGE_EVENT_NAME=%@",BRIDGE_EVENT_NAME);
         
         [methodChannel invokeMethod:BRIDGE_EVENT_NAME arguments:eventData result:^(id  _Nullable result) {
-                    NSLog(@"invokeMethod success result=%@", result);
+            NSLog(@"[page] sendEventToDart invokeMethod success result=%@", result);
         }];
     } else {
-        NSLog(@"invokeMethod methodChannel == null");
+        NSLog(@"[page] sendEventToDart invokeMethod methodChannel == null");
     }
 }
 
 + (void) sendEventToDart2:(LibFlutterBaseMultiplePlugin *)bridgePlugin eventKey:(NSString*) eventKey eventInfo:(NSDictionary*) eventInfo {
     FlutterMethodChannel *methodChannel = bridgePlugin.methodChannel;
-    NSLog(@"sendEventToDart2 methodChannel=%@, eventKey=%@, eventInfo=%@", methodChannel, eventKey, eventInfo);
-    NSString * BRIDGE_EVENT_NAME = @"__codesdancing_flutter_event__";
+    NSLog(@"[page] sendEventToDart2 methodChannel=%@, eventKey=%@, eventInfo=%@", methodChannel, eventKey, eventInfo);
     if (methodChannel != nil) {
         NSMutableDictionary* eventData = NSMutableDictionary.new;
         [eventData setValue:eventKey forKey:@"eventName"];
         [eventData setValue:eventInfo forKey:@"eventInfo"];
   
-        NSLog(@"sendEventToDart2 invokeMethod start BRIDGE_EVENT_NAME=%@",BRIDGE_EVENT_NAME);
+        NSLog(@"[page] sendEventToDart2 invokeMethod start BRIDGE_EVENT_NAME=%@",BRIDGE_EVENT_NAME);
         
         [methodChannel invokeMethod:BRIDGE_EVENT_NAME arguments:eventData result:^(id  _Nullable result) {
-                    NSLog(@"invokeMethod2 success result=%@", result);
+            NSLog(@"[page] sendEventToDart2 invokeMethod2 success result=%@", result);
         }];
     } else {
-        NSLog(@"invokeMethod2 methodChannel == null");
+        NSLog(@"[page] sendEventToDart2 invokeMethod2 methodChannel == null");
     }
 }
 @end
--- mobile/flutter/lib_flutter_base/ios/Classes/FlutterBase/Plugins/BridgeCompact/STFlutterBridgeCompactPlugin.m
+++ mobile/flutter/lib_flutter_base/ios/Classes/FlutterBase/Plugins/BridgeCompact/STFlutterBridgeCompactPlugin.m
@@ -10,24 +10,27 @@
 #import "STToastUtil.h"
 #import "STBridgeDefaultCommunication.h"
 #import "STValueUtil.h"
+#import "STInitializer.h"
 
 @implementation STFlutterBridgeCompactPlugin
 
-- (void)callFunction:(NSString *)functionName
-           arguments:(id)arguments
-              result:(FlutterResult)result{
-
+- (void)callFunction:(NSString *)functionName arguments:(id)arguments result:(FlutterResult)result{
     NSDictionary *parameters = arguments;
 
     if ([functionName isEqualToString:@"callNative"]) {
-        NSString * methodName = [STValueUtil convertToNilIfNull: [parameters valueForKey:@"functionName"]];
+        NSString * functionName = [STValueUtil convertToNilIfNull: [parameters valueForKey:@"functionName"]];
         NSString * params = [STValueUtil convertToNilIfNull: [parameters valueForKey:@"params"]];
-        NSLog(@"functionName methodName=%@, params=%@",methodName, params);
+        NSLog(@"functionName functionName=%@, params=%@",functionName, params);
 
-        [STBridgeDefaultCommunication handleBridge:nil functionName:methodName params:params callBackId:nil callback:^(NSString * _Nullable callBackId, NSString * _Nullable resultJsonString) {
-                    result(resultJsonString);
-        }];
-       
+        if([STInitializer sharedInstance].configBridge.bridgeHandler){
+            [STInitializer sharedInstance].configBridge.bridgeHandler(nil ,functionName ,params ,nil ,^(NSString * _Nullable callBackId, NSString * _Nullable resultJsonString) {
+                result(resultJsonString);
+            });
+        }else{
+            [STBridgeDefaultCommunication handleBridge:nil functionName:functionName params:params callBackId:nil callback:^(NSString * _Nullable callBackId, NSString * _Nullable resultJsonString) {
+                result(resultJsonString);
+            }];
+        }
     } else {
         result(FlutterMethodNotImplemented);
     }

da69ab6a914fb351ee382ec3cc01c399b7d7a0da
kr.mao <kr.mao@ctrip.com>
2021-06-16 11:20:43 +0800

    admin client

--- service/springcloud/components/springcloud-discovery/eureka-server/src/main/kotlin/com/smart/springcloud/discovery/eurekaserver/EurekaServerApplication.kt
+++ service/springcloud/components/springcloud-discovery/eureka-server/src/main/kotlin/com/smart/springcloud/discovery/eurekaserver/EurekaServerApplication.kt
@@ -2,12 +2,14 @@ package com.smart.springcloud.discovery.eurekaserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 /**
  * 用于内部 eureka-client 之间互相访问
  */
 @SpringBootApplication
+//@EnableEurekaClient
 @EnableEurekaServer // 开启 eureka 服务
 class EurekaServerApplication
 

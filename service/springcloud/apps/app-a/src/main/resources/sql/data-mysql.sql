3697feed646575bcda20ab58ca01397e4d701aea
kr.mao <kr.mao@ctrip.com>
2021-06-17 19:04:01 +0800

    optimized mysql

--- service/springcloud/apps/app-a/src/main/resources/sql/data-mysql.sql
+++ service/springcloud/apps/app-a/src/main/resources/sql/data-mysql.sql
@@ -1,3 +1,5 @@
-INSERT INTO `user` (`userId`, `userName`, `roleId`, `password`, `email`, `birthday`, `phone`, `identityNo`, `sex`, `image`, `joinTime`, `loginTime`, `lastPasswordResetTime`, `status`)
-VALUES (1, 'bb', 1, '123', NULL, NULL, '', '', 0, '', '2021-06-17 17:26:34', '2021-06-17 17:26:34', '2021-06-17 17:26:34', 0),
-       (2, 'cc', 2, '123', NULL, NULL, '', '', 0, '', '2021-06-17 17:26:44', '2021-06-17 17:26:44', '2021-06-17 17:26:44', 0);
+INSERT IGNORE INTO USER ( `userId`, `userName`, `roleId`, `password`, `email`, `birthday`, `phone`, `identityNo`, `sex`, `image`, `joinTime`, `loginTime`, `lastPasswordResetTime`, `status` )
+VALUES
+( 1, 'XiaoHong', 1, '123', NULL, NULL, '', '', 0, '', '2021-06-17 17:26:34', '2021-06-17 17:26:34', '2021-06-17 17:26:34', 0 ),
+( 2, 'XiaoHei', 1, '234', NULL, NULL, '', '', 0, '', '2021-06-17 17:26:34', '2021-06-17 17:26:34', '2021-06-17 17:26:34', 0 ),
+( 3, 'XiaoBai', 2, '345', NULL, NULL, '', '', 0, '', '2021-06-17 17:26:44', '2021-06-17 17:26:44', '2021-06-17 17:26:44', 0 );

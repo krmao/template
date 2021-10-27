68706de38eeef0f99d2d9bff0b4fb72a0f3f261e
kr.mao <kr.mao@ctrip.com>
2021-10-27 18:46:01 +0800

    prettier

--- pc/reactapp-antd-pc/src/App.js
+++ pc/reactapp-antd-pc/src/App.js
@@ -1,21 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import MapAddressBaidu from "./MapComponentBaidu";
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
+                <img src={logo} className="App-logo" alt="logo" />
                 <p>
                     Edit <code>src/App.js</code> and save to reload.
                 </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
                     Learn React
                 </a>
                 <div style={{width: "800px", height: "300px", margin: "30px", zIndex: 0}}>
@@ -23,8 +18,7 @@ function App() {
                         style={{width: "100%", height: "100%", marginTop: "10px", zIndex: 0}}
                         centerLatLng={{lat: 31.14826, lng: 121.67196}}
                         zoomLevel={14}
-                        onMapClickListener={(e) => {
-                        }}
+                        onMapClickListener={(e) => {}}
                     />
                 </div>
             </header>
--- pc/reactapp-antd-pc/src/MapComponentBaidu.jsx
+++ pc/reactapp-antd-pc/src/MapComponentBaidu.jsx
@@ -4,7 +4,6 @@ import Map, {MapApiLoaderHOC} from "react-bmapgl/Map";
 import Marker from "react-bmapgl/Overlay/Marker";
 
 class MapAddressBaidu extends React.Component {
-
     render() {
         let centerLatLng = {lat: 31.14826, lng: 121.67196};
         return (
@@ -12,9 +11,11 @@ class MapAddressBaidu extends React.Component {
                 <Map
                     style={{width: "100%", height: "100%", marginTop: "10px", zIndex: 0}}
                     center={centerLatLng}
-                    enableScrollWheelZoom={true} zoom={14}>
-                    <ZoomControl/>
-                    <Marker position={centerLatLng} enableDragging/>
+                    enableScrollWheelZoom={true}
+                    zoom={14}
+                >
+                    <ZoomControl />
+                    <Marker position={centerLatLng} enableDragging />
                 </Map>
             </React.Fragment>
         );
